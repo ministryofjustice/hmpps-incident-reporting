05f3d7a1fa50e78b32687b6690b2faeaeac5916c
ushkarev <ushkarev@users.noreply.github.com>
2024-02-29 10:16:35 +0000

    IR-68: Stricter types in integration testing (#40)

--- integration_tests/mockApis/wiremock.ts
+++ integration_tests/mockApis/wiremock.ts
@@ -1,13 +1,12 @@
-import superagent, { SuperAgentRequest, Response } from 'superagent'
+import superagent, { type SuperAgentRequest, type Response } from 'superagent'
 
-const url = 'http://localhost:9091/__admin'
+const wiremockAdminUrl = 'http://localhost:9091/__admin'
 
-const stubFor = (mapping: Record<string, unknown>): SuperAgentRequest =>
-  superagent.post(`${url}/mappings`).send(mapping)
+export const stubFor = (mapping: Record<string, unknown>): SuperAgentRequest =>
+  superagent.post(`${wiremockAdminUrl}/mappings`).send(mapping)
 
-const getMatchingRequests = body => superagent.post(`${url}/requests/find`).send(body)
+export const getMatchingRequests = (body: string | object) =>
+  superagent.post(`${wiremockAdminUrl}/requests/find`).send(body)
 
-const resetStubs = (): Promise<Array<Response>> =>
-  Promise.all([superagent.delete(`${url}/mappings`), superagent.delete(`${url}/requests`)])
-
-export { stubFor, getMatchingRequests, resetStubs }
+export const resetStubs = (): Promise<Array<Response>> =>
+  Promise.all([superagent.delete(`${wiremockAdminUrl}/mappings`), superagent.delete(`${wiremockAdminUrl}/requests`)])

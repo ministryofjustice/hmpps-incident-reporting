7d6a247a5eb19e6ea9aeb594601c91cf29a42381
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-08-01 17:33:15 +0100

    Added basic event/report types and parse dates

--- server/data/incidentReportingApi.ts
+++ server/data/incidentReportingApi.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line max-classes-per-file
 import config from '../config'
+import { convertEventWithBasicReportsDates } from './incidentReportingApiUtils'
 import RestClient from './restClient'
 
 /**
@@ -22,12 +23,61 @@ export class ErrorResponse {
   }
 }
 
+export type Paginated<T> = {
+  content: T[]
+  number: number
+  size: number
+  numberOfElements: number
+  totalPages: number
+  sort: string[]
+}
+
+export type PaginatedEventsWithBasicReports = Paginated<EventWithBasicReports>
+
+export type EventWithBasicReports = {
+  id: string
+  eventReference: string
+  eventDateAndTime: Date
+  prisonId: string
+  title: string
+  description: string
+  createdAt: Date
+  modifiedAt: Date
+  modifiedBy: string
+  reports: ReportBasic[]
+}
+
+export type ReportBasic = {
+  id: string
+  reportReference: string
+  // TODO: Add enums?
+  type: string
+  incidentDateAndTime: Date
+  prisonId: string
+  title: string
+  description: string
+  reportedBy: string
+  reportedAt: Date
+  // TODO: Add enums?
+  status: string
+  assignedTo: string
+  createdAt: Date
+  modifiedAt: Date
+  modifiedBy: string
+  createdInNomis: boolean
+}
+
 export class IncidentReportingApi extends RestClient {
   constructor(systemToken: string) {
     super('HMPPS Incident Reporting API', config.apis.hmppsIncidentReportingApi, systemToken)
   }
 
-  getEvents(): Promise<object> {
-    return this.get({ path: '/incident-events' })
+  getEvents(): Promise<PaginatedEventsWithBasicReports> {
+    return this.get<DatesAsStrings<PaginatedEventsWithBasicReports>>({ path: '/incident-events' }).then(response => {
+      return {
+        ...response,
+        content: response.content.map(convertEventWithBasicReportsDates),
+      }
+    })
   }
 }

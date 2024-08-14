84135f844ee5be03544078df2552434ba84cf99a
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-08-14 15:51:39 +0100

    Added HMPPS date picker component
    + horizontal-form component

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,7 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 
+import './date-picker'
+
 govukFrontend.initAll()
 mojFrontend.initAll()

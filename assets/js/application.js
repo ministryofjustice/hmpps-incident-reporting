8ad616c4c7e7d01650414da8a647999b68c34713
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-08-16 15:17:27 +0100

    IR-376: Use date picket from MoJ Design System

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,7 +1,5 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 
-import './date-picker'
-
 govukFrontend.initAll()
 mojFrontend.initAll()

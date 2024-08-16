0fdbee726fe2007c21f664d32619741e20f31c1f
Aldo Giambelluca <xoen@users.noreply.github.com>
2024-08-16 15:40:09 +0100

    IR-376: Allow filtering of incidents by dates (#141)
    * Added HMPPS date picker component
    
    + horizontal-form component
    
    * IR-376: Add filtering of incidents by date
    
    * IR-376: Use date picket from MoJ Design System
    
    * IR-376: Removed unnecessary utility function
    
    * IR-376: Removed unsupported field and unused class
    
    in datepicker

--- server/utils/utils.test.ts
+++ server/utils/utils.test.ts
@@ -1,4 +1,13 @@
-import { convertToTitleCase, initialiseName, nameOfPerson, reversedNameOfPerson, toDateString } from './utils'
+import {
+  ErrorSummaryItem,
+  convertToTitleCase,
+  findFieldInErrorSummary,
+  initialiseName,
+  nameOfPerson,
+  parseDateInput,
+  reversedNameOfPerson,
+  toDateString,
+} from './utils'
 
 describe('convert to title case', () => {
   it.each([
@@ -56,6 +65,57 @@ describe('initialise name', () => {
     ['Double barrelled', 'Robert-John Smith-Jones-Wilson', 'R. Smith-Jones-Wilson'],
   ])('%s initialiseName(%s, %s)', (_: string, a: string, expected: string) => {
     expect(initialiseName(a)).toEqual(expected)
+  })
+})
+
+describe('parseDateInput', () => {
+  it.each([
+    ['25/02/2024', [25, 2, 2024]],
+    ['01/01/2024 ', [1, 1, 2024]],
+    ['1/1/2024', [1, 1, 2024]],
+  ])('should work on valid date %s', (input, [day, month, year]) => {
+    const date = parseDateInput(input)
+    expect(date.getDate()).toEqual(day)
+    expect(date.getMonth()).toEqual(month - 1)
+    expect(date.getFullYear()).toEqual(year)
+  })
+
+  it.each([
+    undefined,
+    null,
+    '',
+    '1/1/24',
+    '32/01/2024',
+    '20-01-2024',
+    '01/01/2024 12:00',
+    '2024-01-01',
+    '2024-01-01T12:00:00Z',
+    'today',
+  ])('should throw an error on invalid date %p', input => {
+    expect(() => parseDateInput(input)).toThrow('Invalid date')
+  })
+})
+
+describe('findFieldInErrorSummary', () => {
+  it.each([undefined, null])('should return null if error list is %p', list => {
+    expect(findFieldInErrorSummary(list, 'field')).toBeNull()
+  })
+
+  it('should return null if error list is empty', () => {
+    expect(findFieldInErrorSummary([], 'field')).toBeNull()
+  })
+
+  const errorList: ErrorSummaryItem[] = [
+    { text: 'Enter a number', href: '#field1' },
+    { text: 'Enter a date', href: '#field3' },
+  ]
+
+  it('should return null if field is not found', () => {
+    expect(findFieldInErrorSummary(errorList, 'field2')).toBeNull()
+  })
+
+  it('should return error message if field is found', () => {
+    expect(findFieldInErrorSummary(errorList, 'field3')).toStrictEqual({ text: 'Enter a date' })
   })
 })
 

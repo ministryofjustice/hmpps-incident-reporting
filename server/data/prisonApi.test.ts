f19ff1f8d57367e83172e82084695c7f4349e93d
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-08-20 16:11:52 +0100

    Add prison-api endpoint to retrieve complete set of incident questionnaires

--- server/data/prisonApi.test.ts
+++ server/data/prisonApi.test.ts
@@ -1,12 +1,12 @@
 import nock from 'nock'
 
 import config from '../config'
-import { PrisonApi, type Prison } from './prisonApi'
+import { PrisonApi, type Prison, type IncidentTypeConfiguration } from './prisonApi'
 import { leeds, moorland } from './testData/prisonApi'
 
 jest.mock('./tokenStore/redisTokenStore')
 
-describe('offenderSearchApi', () => {
+describe('prisonApi', () => {
   let fakeApiClient: nock.Scope
   let apiClient: PrisonApi
 
@@ -27,6 +27,130 @@ describe('offenderSearchApi', () => {
       await expect(apiClient.getPrisons()).resolves.toEqual<Record<string, Prison>>({
         LEI: leeds,
         MDI: moorland,
+      })
+    })
+  })
+
+  describe('getIncidentTypeConfiguration', () => {
+    describe('conversion of date fields', () => {
+      it('should work for absent fields', async () => {
+        fakeApiClient
+          .get('/api/incidents/configuration')
+          .query(true)
+          .reply(200, [
+            {
+              incidentType: 'ASSAULT',
+              incidentTypeDescription: 'Assaults',
+              questionnaireId: 1,
+              questions: [
+                {
+                  questionnaireQueId: 2,
+                  questionSeq: 1,
+                  questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+                  questionListSeq: 1,
+                  questionActiveFlag: true,
+                  multipleAnswerFlag: false,
+                  answers: [
+                    {
+                      questionnaireAnsId: 3,
+                      answerSeq: 1,
+                      answerDesc: 'YES (ENTER DATE)',
+                      answerListSeq: 1,
+                      answerActiveFlag: true,
+                      dateRequiredFlag: true,
+                      commentRequiredFlag: false,
+                      nextQuestionnaireQueId: 4,
+                    },
+                  ],
+                },
+              ],
+              prisonerRoles: [
+                {
+                  prisonerRole: 'PRESENT',
+                  singleRole: false,
+                  active: true,
+                },
+              ],
+              active: true,
+            },
+          ] satisfies DatesAsStrings<IncidentTypeConfiguration[]>)
+
+        const incidentTypes = await apiClient.getIncidentTypeConfiguration()
+        expect(incidentTypes).toHaveLength(1)
+        const incidentType = incidentTypes[0]
+        expect(incidentType.questions).toHaveLength(1)
+        const question = incidentType.questions[0]
+        expect(question.answers).toHaveLength(1)
+        const answer = question.answers[0]
+        expect(incidentType.prisonerRoles).toHaveLength(1)
+        const prisonerRole = incidentType.prisonerRoles[0]
+
+        expect(incidentType.expiryDate).toBeUndefined()
+        expect(question.questionExpiryDate).toBeUndefined()
+        expect(answer.answerExpiryDate).toBeUndefined()
+        expect(prisonerRole.expiryDate).toBeUndefined()
+      })
+
+      it('should work for present, nested date fields', async () => {
+        fakeApiClient
+          .get('/api/incidents/configuration')
+          .query(true)
+          .reply(200, [
+            {
+              incidentType: 'ASSAULT',
+              incidentTypeDescription: 'Assaults',
+              questionnaireId: 1,
+              questions: [
+                {
+                  questionnaireQueId: 2,
+                  questionSeq: 1,
+                  questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+                  questionListSeq: 1,
+                  questionActiveFlag: false,
+                  questionExpiryDate: '2022-08-20',
+                  multipleAnswerFlag: false,
+                  answers: [
+                    {
+                      questionnaireAnsId: 3,
+                      answerSeq: 1,
+                      answerDesc: 'YES (ENTER DATE)',
+                      answerListSeq: 1,
+                      answerActiveFlag: false,
+                      answerExpiryDate: '2022-08-20',
+                      dateRequiredFlag: true,
+                      commentRequiredFlag: false,
+                      nextQuestionnaireQueId: 4,
+                    },
+                  ],
+                },
+              ],
+              prisonerRoles: [
+                {
+                  prisonerRole: 'PRESENT',
+                  singleRole: false,
+                  active: false,
+                  expiryDate: '2022-08-20',
+                },
+              ],
+              active: false,
+              expiryDate: '2022-08-20',
+            },
+          ] satisfies DatesAsStrings<IncidentTypeConfiguration[]>)
+
+        const incidentTypes = await apiClient.getIncidentTypeConfiguration()
+        expect(incidentTypes).toHaveLength(1)
+        const incidentType = incidentTypes[0]
+        expect(incidentType.questions).toHaveLength(1)
+        const question = incidentType.questions[0]
+        expect(question.answers).toHaveLength(1)
+        const answer = question.answers[0]
+        expect(incidentType.prisonerRoles).toHaveLength(1)
+        const prisonerRole = incidentType.prisonerRoles[0]
+
+        expect(incidentType.expiryDate).toBeInstanceOf(Date)
+        expect(question.questionExpiryDate).toBeInstanceOf(Date)
+        expect(answer.answerExpiryDate).toBeInstanceOf(Date)
+        expect(prisonerRole.expiryDate).toBeInstanceOf(Date)
       })
     })
   })
--- server/data/prisonApi.ts
+++ server/data/prisonApi.ts
@@ -8,6 +8,78 @@ export type Prison = {
   active: boolean
 }
 
+/** Incident Type Configuration */
+export interface IncidentTypeConfiguration {
+  /** Incident type of this configuration */
+  incidentType: string
+  /** Incident type description */
+  incidentTypeDescription: string
+  /** ID internal of this question set for this incident type */
+  questionnaireId: number
+  /** List of questions (with answers) for this incident type */
+  questions: Question[]
+  /** List of roles that can apply to a prisoner in this incident type */
+  prisonerRoles: PrisonerRole[]
+  /** Indicates this incident type is still usable */
+  active: boolean
+  /** Date the incident type was expired */
+  expiryDate?: Date
+}
+
+/** Incident type question */
+interface Question {
+  /** Question ID */
+  questionnaireQueId: number
+  /** Question sequence number */
+  questionSeq: number
+  /** Question description */
+  questionDesc: string
+  /** Question list sequence */
+  questionListSeq: number
+  /** Indicates question is active */
+  questionActiveFlag: boolean
+  /** Date question was retired */
+  questionExpiryDate?: Date
+  /** Indicate multiple responses can be given */
+  multipleAnswerFlag: boolean
+  /** Order list of answers */
+  answers: Answer[]
+}
+
+/** Incident Type Answer */
+interface Answer {
+  /** ID for this Answer */
+  questionnaireAnsId: number
+  /** Sequence for this answer */
+  answerSeq: number
+  /** Answer Text */
+  answerDesc: string
+  /** Answer Sequence */
+  answerListSeq: number
+  /** Indicates this answer is active */
+  answerActiveFlag: boolean
+  /** Date expired (not used) */
+  answerExpiryDate?: Date
+  /** Should the answer include date information? */
+  dateRequiredFlag: boolean
+  /** Should the answer include comment? */
+  commentRequiredFlag: boolean
+  /** Next questionnaire question ID */
+  nextQuestionnaireQueId?: number
+}
+
+/** Incident type prisoner role */
+interface PrisonerRole {
+  /** Role type for this question set */
+  prisonerRole: string
+  /** If a single party can have this role in the question set */
+  singleRole: boolean
+  /** Indicates this role is active */
+  active: boolean
+  /** Indicates the date the role was made inactive */
+  expiryDate?: Date
+}
+
 export class PrisonApi extends RestClient {
   constructor(systemToken: string) {
     super('HMPPS Prison API', config.apis.hmppsPrisonApi, systemToken)
@@ -20,5 +92,45 @@ export class PrisonApi extends RestClient {
 
     // Returns the prisons in an object for easy access
     return prisons.reduce((prev, prisonInfo) => ({ ...prev, [prisonInfo.agencyId]: prisonInfo }), {})
+  }
+
+  /**
+   * Load current NOMIS configuration for all incident types, including inactive ones.
+   * Optionally, can be filtered to return only 1 type.
+   */
+  getIncidentTypeConfiguration(type?: string): Promise<IncidentTypeConfiguration[]> {
+    return this.get<DatesAsStrings<IncidentTypeConfiguration>[]>({
+      path: '/api/incidents/configuration',
+      query: { 'incident-type': type },
+    }).then(incidentTypes =>
+      incidentTypes.map(
+        incidentType =>
+          ({
+            ...incidentType,
+            questions: incidentType.questions.map(
+              question =>
+                ({
+                  ...question,
+                  questionExpiryDate: question.questionExpiryDate && new Date(question.questionExpiryDate),
+                  answers: question.answers.map(
+                    answer =>
+                      ({
+                        ...answer,
+                        answerExpiryDate: answer.answerExpiryDate && new Date(answer.answerExpiryDate),
+                      }) satisfies Answer,
+                  ),
+                }) satisfies Question,
+            ),
+            prisonerRoles: incidentType.prisonerRoles.map(
+              prisonerRole =>
+                ({
+                  ...prisonerRole,
+                  expiryDate: prisonerRole.expiryDate && new Date(prisonerRole.expiryDate),
+                }) satisfies PrisonerRole,
+            ),
+            expiryDate: incidentType.expiryDate && new Date(incidentType.expiryDate),
+          }) satisfies IncidentTypeConfiguration,
+      ),
+    )
   }
 }

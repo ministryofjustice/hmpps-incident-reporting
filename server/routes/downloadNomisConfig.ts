47c5654187b39a209f3cc09f20e3c79bd2694246
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-08-21 15:04:16 +0100

    Factor out CSV streaming utility

--- server/routes/downloadNomisConfig.ts
+++ server/routes/downloadNomisConfig.ts
@@ -2,22 +2,59 @@ import type { RequestHandler, Response } from 'express'
 
 import type { ReferenceCode } from '../data/prisonApi'
 
-function streamReferenceCodeResponse(fileName: string, referenceCodes: ReferenceCode[], res: Response): void {
+type CsvCellValue = string | number | boolean | null
+
+function streamCsvDownload(res: Response, fileName: string, lines: Generator<CsvCellValue[]>): void {
   res.header('Content-Type', 'text/csv')
   res.header('Content-Disposition', `attachment; filename="${fileName}"`)
-  res.write('Domain,Code,Description,Sequence,Active,System\n')
-  referenceCodes.forEach(referenceCode => {
-    const fields = [
+  for (const line of lines) {
+    const lineString = line.map(escapeCsvCell).join(',')
+    res.write(`${lineString}\n`)
+  }
+  res.end()
+}
+
+function escapeCsvCell(value: CsvCellValue): string {
+  /* eslint-disable no-param-reassign */
+  if (value === null || value === undefined) {
+    return ''
+  }
+  let mustQuote = false
+  if (value === true) {
+    return 'TRUE'
+  }
+  if (value === false) {
+    return 'FALSE'
+  }
+  if (typeof value === 'number') {
+    value = value.toString()
+  }
+  if (value.includes(',')) {
+    mustQuote = true
+  }
+  if (value.includes('"')) {
+    value = value.replaceAll('"', '""')
+    mustQuote = true
+  }
+  if (mustQuote) {
+    return `"${value}"`
+  }
+  return value
+  /* eslint-enable no-param-reassign */
+}
+
+function* mapCodes(referenceCodes: ReferenceCode[]) {
+  yield ['Domain', 'Code', 'Description', 'Sequence', 'Active', 'System']
+  for (const referenceCode of referenceCodes) {
+    yield [
       referenceCode.domain,
       referenceCode.code,
       referenceCode.description,
-      referenceCode.listSeq?.toString() ?? '',
-      referenceCode.activeFlag === 'N' ? 'FALSE' : 'TRUE',
-      referenceCode.systemDataFlag === 'N' ? 'FALSE' : 'TRUE',
+      referenceCode.listSeq,
+      referenceCode.activeFlag === 'Y',
+      referenceCode.systemDataFlag === 'Y',
     ]
-    res.write(`${fields.join(',')}\n`)
-  })
-  res.end()
+  }
 }
 
 export default function makeDownloadNomisConfigRoutes(): Record<string, RequestHandler> {
@@ -26,28 +63,28 @@ export default function makeDownloadNomisConfigRoutes(): Record<string, RequestH
       const { prisonApi } = res.locals.apis
       prisonApi
         .getIncidentTypes()
-        .then(referenceCodes => streamReferenceCodeResponse('incident-types.csv', referenceCodes, res))
+        .then(referenceCodes => streamCsvDownload(res, 'incident-types.csv', mapCodes(referenceCodes)))
     },
 
     staffInvolvementRoles(req, res): void {
       const { prisonApi } = res.locals.apis
       prisonApi
         .getStaffInvolvementRoles()
-        .then(referenceCodes => streamReferenceCodeResponse('staff-involvement-roles.csv', referenceCodes, res))
+        .then(referenceCodes => streamCsvDownload(res, 'staff-involvement-roles.csv', mapCodes(referenceCodes)))
     },
 
     prisonerInvolvementRoles(req, res): void {
       const { prisonApi } = res.locals.apis
       prisonApi
         .getPrisonerInvolvementRoles()
-        .then(referenceCodes => streamReferenceCodeResponse('prisoner-involvement-roles.csv', referenceCodes, res))
+        .then(referenceCodes => streamCsvDownload(res, 'prisoner-involvement-roles.csv', mapCodes(referenceCodes)))
     },
 
     prisonerInvolvementOutcome(req, res): void {
       const { prisonApi } = res.locals.apis
       prisonApi
         .getPrisonerInvolvementOutcome()
-        .then(referenceCodes => streamReferenceCodeResponse('prisoner-involvement-outcome.csv', referenceCodes, res))
+        .then(referenceCodes => streamCsvDownload(res, 'prisoner-involvement-outcome.csv', mapCodes(referenceCodes)))
     },
   }
 }

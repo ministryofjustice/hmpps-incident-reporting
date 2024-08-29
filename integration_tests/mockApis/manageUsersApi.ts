14f2efa9cfc8c1e6cbe3427d2d6a6380771614c9
ushkarev <ushkarev@users.noreply.github.com>
2024-08-29 14:22:03 +0100

    IR-429: Integration test utilities (#152)
    * Add incomplete wiremock stub mapping type
    * Rename cypress task that resets wiremock stubs
    * Add wiremock stubs that will be needed in future
    * Include custom types during integration test type checking

--- integration_tests/mockApis/manageUsersApi.ts
+++ integration_tests/mockApis/manageUsersApi.ts
@@ -1,9 +1,11 @@
 import type { SuperAgentRequest } from 'superagent'
 
 import { stubFor } from './wiremock'
+import type { User } from '../../server/data/manageUsersApiClient'
 
 export default {
-  stubManageUser: (name: string = 'john smith'): SuperAgentRequest =>
+  /** Current user */
+  stubManageUserMe: (name: string = 'john smith'): SuperAgentRequest =>
     stubFor({
       request: {
         method: 'GET',
@@ -19,9 +21,31 @@ export default {
           username: 'USER1',
           active: true,
           name,
-        },
+        } satisfies User,
       },
     }),
+
+  /** Another user, looked up by username */
+  stubManageUserNamed: (username: string = 'jsmith', name: string = 'john smith'): SuperAgentRequest =>
+    stubFor({
+      request: {
+        method: 'GET',
+        urlPath: `/manage-users-api/users/${encodeURIComponent(username)}`,
+      },
+      response: {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8',
+        },
+        jsonBody: {
+          staffId: 231232,
+          username: 'USER1',
+          active: true,
+          name,
+        } satisfies User,
+      },
+    }),
+
   stubManageUsersPing: (): SuperAgentRequest =>
     stubFor({
       request: {

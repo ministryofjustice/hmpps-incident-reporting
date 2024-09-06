6443aa30ef938684461a0085b1d1dae0edf1681e
Thomas-Geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2024-09-06 12:29:47 +0100

    Merge populateCurrentUser and setupCurrentUser (hmpps-template-typescript#415)

--- server/middleware/setUpCurrentUser.ts
+++ server/middleware/setUpCurrentUser.ts
@@ -1,10 +1,27 @@
 import { Router } from 'express'
 
+import logger from '../../logger'
 import type { Services } from '../services'
-import populateCurrentUser from './populateCurrentUser'
 
 export default function setUpCurrentUser({ userService }: Services): Router {
-  const router = Router({ mergeParams: true })
-  router.use(populateCurrentUser(userService))
+  const router = Router()
+
+  router.use(async (req, res, next) => {
+    try {
+      if (res.locals.user) {
+        const user = await userService.getUser(res.locals.user.token)
+        if (user) {
+          res.locals.user = { ...user, ...res.locals.user }
+        } else {
+          logger.info('No user available')
+        }
+      }
+      next()
+    } catch (error) {
+      logger.error(error, `Failed to retrieve user for: ${res.locals.user && res.locals.user.username}`)
+      next(error)
+    }
+  })
+
   return router
 }

eaa0614c716009b947f825ace6be9a6f57a59957
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-09-11 00:44:05 +0100

    Allow downloading incident reporting api constants as source JSON

---
+++ server/routes/downloadReportConfig/dps.ts
@@ -0,0 +1,26 @@
+import { Router, type Request, type Response } from 'express'
+
+import { jsonDownload } from './utils'
+
+export default function makeDpsConfigRouter(): Router {
+  const router = Router()
+
+  ;[
+    { method: 'types' as const },
+    { method: 'statuses' as const },
+    { method: 'informationSources' as const },
+    { method: 'staffInvolvementRoles' as const },
+    { method: 'prisonerInvolvementRoles' as const },
+    { method: 'prisonerInvolvementOutcomes' as const },
+    { method: 'correctionRequestReasons' as const },
+    { method: 'errorCodes' as const },
+  ].forEach(({ method }) => {
+    router.get(`/${method}.json`, async (_req: Request, res: Response) => {
+      const { incidentReportingApi } = res.locals.apis
+      const constants = await incidentReportingApi.constants[method]()
+      jsonDownload(res, method, constants)
+    })
+  })
+
+  return router
+}

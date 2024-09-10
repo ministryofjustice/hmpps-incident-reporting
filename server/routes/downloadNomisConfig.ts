e406956f58ba02de4d9fc84e1bdde8bfbffe8c8e
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-09-10 20:18:17 +0100

    Refactor NOMIS config download routes into a self-contained nested router

--- server/routes/downloadNomisConfig.ts
+++ server/routes/downloadNomisConfig.ts
@@ -1,4 +1,4 @@
-import type { RequestHandler, Response } from 'express'
+import { Router, type Request, type Response } from 'express'
 import { NotFound } from 'http-errors'
 
 import type { ReferenceCode } from '../data/prisonApi'
@@ -45,6 +45,129 @@ function escapeCsvCell(value: CsvCellValue): string {
   /* eslint-enable no-param-reassign */
 }
 
+async function downloadIncidentTypes(req: Request, res: Response): Promise<void> {
+  const { prisonApi } = res.locals.apis
+  const incidentTypes = await prisonApi.getIncidentTypeConfiguration()
+
+  function* mapTypes(): Generator<CsvCellValue[]> {
+    yield ['Type', 'Description', 'Questionnaire ID', 'Active', 'Expired']
+
+    for (const incidentType of incidentTypes) {
+      yield [
+        incidentType.incidentType,
+        incidentType.incidentTypeDescription,
+        incidentType.questionnaireId,
+        incidentType.active,
+        format.shortDate(incidentType.expiryDate),
+      ]
+    }
+  }
+
+  return streamCsvDownload(res, 'incident-types.csv', mapTypes())
+}
+
+async function downloadIncidentTypeQuestions(req: Request, res: Response): Promise<void> {
+  const { type } = req.params
+  if (!type) {
+    throw new NotFound()
+  }
+
+  const { prisonApi } = res.locals.apis
+  const incidentTypes = await prisonApi.getIncidentTypeConfiguration(type)
+
+  function* mapType(): Generator<CsvCellValue[]> {
+    if (incidentTypes.length !== 1) {
+      yield ['Type not found', type]
+      return
+    }
+
+    yield [
+      // question headers
+      'Question ID',
+      'Question sequence',
+      'Question list sequence',
+      'Question',
+      'Allows multiple answers?',
+      'Question is active',
+      'Question expired',
+      // answer headers
+      'Answer ID',
+      'Answer sequence',
+      'Answer list sequence',
+      'Answer',
+      'Answer requires comment',
+      'Answer requires date',
+      'Answer is active',
+      'Answer expired',
+      'Next question ID',
+    ]
+
+    for (const question of incidentTypes[0].questions) {
+      yield [
+        question.questionnaireQueId,
+        question.questionSeq,
+        question.questionListSeq,
+        question.questionDesc,
+        question.multipleAnswerFlag,
+        question.questionActiveFlag,
+        format.shortDate(question.questionExpiryDate),
+        ...Array(8),
+      ]
+      for (const answer of question.answers) {
+        yield [
+          ...Array(7),
+          answer.questionnaireAnsId,
+          answer.answerSeq,
+          answer.answerListSeq,
+          answer.answerDesc,
+          answer.answerActiveFlag,
+          answer.commentRequiredFlag,
+          answer.dateRequiredFlag,
+          format.shortDate(answer.answerExpiryDate),
+          answer.nextQuestionnaireQueId ?? 'None',
+        ]
+      }
+    }
+  }
+
+  return streamCsvDownload(res, `incident-type-questions-${type}.csv`, mapType())
+}
+
+async function downloadIncidentTypePrisonerRoles(req: Request, res: Response): Promise<void> {
+  const { type } = req.params
+  if (!type) {
+    throw new NotFound()
+  }
+
+  const { prisonApi } = res.locals.apis
+  const [prisonerInvolvementRoles, incidentTypes] = await Promise.all([
+    prisonApi.getPrisonerInvolvementRoles(),
+    prisonApi.getIncidentTypeConfiguration(type),
+  ])
+  const prisonerInvolvementMap = new Map(prisonerInvolvementRoles.map(code => [code.code, code.description]))
+
+  function* mapType(): Generator<CsvCellValue[]> {
+    if (incidentTypes.length !== 1) {
+      yield ['Type not found', type]
+      return
+    }
+
+    yield ['Role', 'Description (from reference data)', 'Only one prisoner can have this role', 'Active', 'Expired']
+
+    for (const prisonerRole of incidentTypes[0].prisonerRoles) {
+      yield [
+        prisonerRole.prisonerRole,
+        prisonerInvolvementMap.get(prisonerRole.prisonerRole) ?? '',
+        prisonerRole.singleRole,
+        prisonerRole.active,
+        format.shortDate(prisonerRole.expiryDate),
+      ]
+    }
+  }
+
+  return streamCsvDownload(res, `incident-type-prisoner-roles-${type}.csv`, mapType())
+}
+
 function* mapCodes(referenceCodes: ReferenceCode[]) {
   yield ['Domain', 'Code', 'Description', 'Sequence', 'Active', 'System']
 
@@ -60,147 +183,33 @@ function* mapCodes(referenceCodes: ReferenceCode[]) {
   }
 }
 
-export default function makeDownloadNomisConfigRoutes(): Record<string, RequestHandler> {
-  return {
-    async incidentTypes(req, res): Promise<void> {
-      const { prisonApi } = res.locals.apis
-      const incidentTypes = await prisonApi.getIncidentTypeConfiguration()
+async function downloadStaffInvolvementRoles(req: Request, res: Response): Promise<void> {
+  const { prisonApi } = res.locals.apis
+  const referenceCodes = await prisonApi.getStaffInvolvementRoles()
+  return streamCsvDownload(res, 'staff-involvement-roles.csv', mapCodes(referenceCodes))
+}
 
-      function* mapTypes(): Generator<CsvCellValue[]> {
-        yield ['Type', 'Description', 'Questionnaire ID', 'Active', 'Expired']
+async function downloadPrisonerInvolvementRoles(req: Request, res: Response): Promise<void> {
+  const { prisonApi } = res.locals.apis
+  const referenceCodes = await prisonApi.getPrisonerInvolvementRoles()
+  return streamCsvDownload(res, 'prisoner-involvement-roles.csv', mapCodes(referenceCodes))
+}
 
-        for (const incidentType of incidentTypes) {
-          yield [
-            incidentType.incidentType,
-            incidentType.incidentTypeDescription,
-            incidentType.questionnaireId,
-            incidentType.active,
-            format.shortDate(incidentType.expiryDate),
-          ]
-        }
-      }
+async function downloadPrisonerInvolvementOutcome(req: Request, res: Response): Promise<void> {
+  const { prisonApi } = res.locals.apis
+  const referenceCodes = await prisonApi.getPrisonerInvolvementOutcome()
+  return streamCsvDownload(res, 'prisoner-involvement-outcome.csv', mapCodes(referenceCodes))
+}
 
-      return streamCsvDownload(res, 'incident-types.csv', mapTypes())
-    },
+export default function makeNomisConfigRouter(): Router {
+  const router = Router()
 
-    async incidentTypeQuestions(req, res): Promise<void> {
-      const { type } = req.params
-      if (!type) {
-        throw new NotFound()
-      }
+  router.get('/incident-types.csv', downloadIncidentTypes)
+  router.get('/incident-type/:type/questions.csv', downloadIncidentTypeQuestions)
+  router.get('/incident-type/:type/prisoner-roles.csv', downloadIncidentTypePrisonerRoles)
+  router.get('/staff-involvement-roles.csv', downloadStaffInvolvementRoles)
+  router.get('/prisoner-involvement-roles.csv', downloadPrisonerInvolvementRoles)
+  router.get('/prisoner-involvement-outcome.csv', downloadPrisonerInvolvementOutcome)
 
-      const { prisonApi } = res.locals.apis
-      const incidentTypes = await prisonApi.getIncidentTypeConfiguration(type)
-
-      function* mapType(): Generator<CsvCellValue[]> {
-        if (incidentTypes.length !== 1) {
-          yield ['Type not found', type]
-          return
-        }
-
-        yield [
-          // question headers
-          'Question ID',
-          'Question sequence',
-          'Question list sequence',
-          'Question',
-          'Allows multiple answers?',
-          'Question is active',
-          'Question expired',
-          // answer headers
-          'Answer ID',
-          'Answer sequence',
-          'Answer list sequence',
-          'Answer',
-          'Answer requires comment',
-          'Answer requires date',
-          'Answer is active',
-          'Answer expired',
-          'Next question ID',
-        ]
-
-        for (const question of incidentTypes[0].questions) {
-          yield [
-            question.questionnaireQueId,
-            question.questionSeq,
-            question.questionListSeq,
-            question.questionDesc,
-            question.multipleAnswerFlag,
-            question.questionActiveFlag,
-            format.shortDate(question.questionExpiryDate),
-            ...Array(8),
-          ]
-          for (const answer of question.answers) {
-            yield [
-              ...Array(7),
-              answer.questionnaireAnsId,
-              answer.answerSeq,
-              answer.answerListSeq,
-              answer.answerDesc,
-              answer.answerActiveFlag,
-              answer.commentRequiredFlag,
-              answer.dateRequiredFlag,
-              format.shortDate(answer.answerExpiryDate),
-              answer.nextQuestionnaireQueId ?? 'None',
-            ]
-          }
-        }
-      }
-
-      return streamCsvDownload(res, `incident-type-questions-${type}.csv`, mapType())
-    },
-
-    async incidentTypePrisonerRoles(req, res): Promise<void> {
-      const { type } = req.params
-      if (!type) {
-        throw new NotFound()
-      }
-
-      const { prisonApi } = res.locals.apis
-      const [prisonerInvolvementRoles, incidentTypes] = await Promise.all([
-        prisonApi.getPrisonerInvolvementRoles(),
-        prisonApi.getIncidentTypeConfiguration(type),
-      ])
-      const prisonerInvolvementMap = new Map(prisonerInvolvementRoles.map(code => [code.code, code.description]))
-
-      function* mapType(): Generator<CsvCellValue[]> {
-        if (incidentTypes.length !== 1) {
-          yield ['Type not found', type]
-          return
-        }
-
-        yield ['Role', 'Description (from reference data)', 'Only one prisoner can have this role', 'Active', 'Expired']
-
-        for (const prisonerRole of incidentTypes[0].prisonerRoles) {
-          yield [
-            prisonerRole.prisonerRole,
-            prisonerInvolvementMap.get(prisonerRole.prisonerRole) ?? '',
-            prisonerRole.singleRole,
-            prisonerRole.active,
-            format.shortDate(prisonerRole.expiryDate),
-          ]
-        }
-      }
-
-      return streamCsvDownload(res, `incident-type-prisoner-roles-${type}.csv`, mapType())
-    },
-
-    async staffInvolvementRoles(req, res): Promise<void> {
-      const { prisonApi } = res.locals.apis
-      const referenceCodes = await prisonApi.getStaffInvolvementRoles()
-      return streamCsvDownload(res, 'staff-involvement-roles.csv', mapCodes(referenceCodes))
-    },
-
-    async prisonerInvolvementRoles(req, res): Promise<void> {
-      const { prisonApi } = res.locals.apis
-      const referenceCodes = await prisonApi.getPrisonerInvolvementRoles()
-      return streamCsvDownload(res, 'prisoner-involvement-roles.csv', mapCodes(referenceCodes))
-    },
-
-    async prisonerInvolvementOutcome(req, res): Promise<void> {
-      const { prisonApi } = res.locals.apis
-      const referenceCodes = await prisonApi.getPrisonerInvolvementOutcome()
-      return streamCsvDownload(res, 'prisoner-involvement-outcome.csv', mapCodes(referenceCodes))
-    },
-  }
+  return router
 }

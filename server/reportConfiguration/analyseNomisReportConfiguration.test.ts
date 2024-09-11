17410cbcfd7436a14be0a25dfab6114041c2b6d9
Igor <igor.ushkarev@digital.justice.gov.uk>
2024-09-11 22:43:48 +0100

    Have NOMIS report analysis check ID and type uniqueness of report configurations, questions and answers

--- server/reportConfiguration/analyseNomisReportConfiguration.test.ts
+++ server/reportConfiguration/analyseNomisReportConfiguration.test.ts
@@ -1,39 +1,8 @@
 import { analyseNomisReportConfiguration } from './analyseNomisReportConfiguration'
 
 describe('Analysing NOMIS report configuration', () => {
-  describe('type uniqueness', () => {
-    it('should return an error if some types are not unique', () => {
-      const results = analyseNomisReportConfiguration([
-        {
-          incidentType: 'ASSAULT',
-          incidentTypeDescription: 'Assaults 2020',
-          questionnaireId: 1,
-          questions: [],
-          prisonerRoles: [],
-          active: true,
-        },
-        {
-          incidentType: 'ASSAULT1',
-          incidentTypeDescription: 'Assaults 2021',
-          questionnaireId: 2,
-          questions: [],
-          prisonerRoles: [],
-          active: true,
-        },
-        {
-          incidentType: 'ASSAULT',
-          incidentTypeDescription: 'Assaults 2022',
-          questionnaireId: 3,
-          questions: [],
-          prisonerRoles: [],
-          active: true,
-        },
-      ])
-      expect(results).toHaveLength(1)
-      expect(results[0]).toHaveProperty('message', 'There are 3 configurations but 2 unique type codes')
-    })
-
-    it('should pass if all types are unique', () => {
+  describe('id & type uniqueness', () => {
+    it('should return an error if some ids are not unique', () => {
       const results = analyseNomisReportConfiguration([
         {
           incidentType: 'ASSAULT',
@@ -54,8 +23,390 @@ describe('Analysing NOMIS report configuration', () => {
         {
           incidentType: 'ASSAULT2',
           incidentTypeDescription: 'Assaults 2022',
+          questionnaireId: 2, // DUPLICATE
+          questions: [],
+          prisonerRoles: [],
+          active: true,
+        },
+      ])
+      expect(results).toHaveLength(1)
+      expect(results[0]).toHaveProperty('message', 'There are 3 configurations but 2 unique ids')
+    })
+
+    it('should return an error if some types are not unique', () => {
+      const results = analyseNomisReportConfiguration([
+        {
+          incidentType: 'ASSAULT',
+          incidentTypeDescription: 'Assaults 2020',
+          questionnaireId: 1,
+          questions: [],
+          prisonerRoles: [],
+          active: true,
+        },
+        {
+          incidentType: 'ASSAULT1',
+          incidentTypeDescription: 'Assaults 2021',
+          questionnaireId: 2,
+          questions: [],
+          prisonerRoles: [],
+          active: true,
+        },
+        {
+          incidentType: 'ASSAULT', // DUPLICATE
+          incidentTypeDescription: 'Assaults 2022',
           questionnaireId: 3,
           questions: [],
+          prisonerRoles: [],
+          active: true,
+        },
+      ])
+      expect(results).toHaveLength(1)
+      expect(results[0]).toHaveProperty('message', 'There are 3 configurations but 2 unique types')
+    })
+
+    it('should return an error if some question ids are not unique', () => {
+      const results = analyseNomisReportConfiguration([
+        {
+          incidentType: 'ASSAULT1',
+          incidentTypeDescription: 'Assaults 2021',
+          questionnaireId: 2,
+          questions: [
+            {
+              questionnaireQueId: 1,
+              questionSeq: 1,
+              questionDesc: 'WAS HEALTHCARE INVOLVED',
+              questionListSeq: 1,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [],
+            },
+            {
+              questionnaireQueId: 2,
+              questionSeq: 2,
+              questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+              questionListSeq: 2,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [],
+            },
+          ],
+          prisonerRoles: [],
+          active: true,
+        },
+        {
+          incidentType: 'ASSAULT',
+          incidentTypeDescription: 'Assaults 2022',
+          questionnaireId: 3,
+          questions: [
+            {
+              questionnaireQueId: 3,
+              questionSeq: 1,
+              questionDesc: 'WAS HEALTHCARE INVOLVED',
+              questionListSeq: 1,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [],
+            },
+            {
+              questionnaireQueId: 3, // DUPLICATE
+              questionSeq: 2,
+              questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+              questionListSeq: 2,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [],
+            },
+          ],
+          prisonerRoles: [],
+          active: true,
+        },
+      ])
+      expect(results).toHaveLength(1)
+      expect(results[0]).toHaveProperty('message', 'There are 4 questions but 3 unique question ids')
+    })
+
+    it('should return an error if some answer ids are not unique', () => {
+      const results = analyseNomisReportConfiguration([
+        {
+          incidentType: 'ASSAULT1',
+          incidentTypeDescription: 'Assaults 2021',
+          questionnaireId: 2,
+          questions: [
+            {
+              questionnaireQueId: 1,
+              questionSeq: 1,
+              questionDesc: 'WAS HEALTHCARE INVOLVED',
+              questionListSeq: 1,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 1,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER STAFF NAME)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: true,
+                  nextQuestionnaireQueId: 2,
+                },
+                {
+                  questionnaireAnsId: 2,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: 2,
+                },
+              ],
+            },
+            {
+              questionnaireQueId: 2,
+              questionSeq: 2,
+              questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+              questionListSeq: 2,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 3,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER DATE)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: true,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+                {
+                  questionnaireAnsId: 2, // DUPLICATE
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+              ],
+            },
+          ],
+          prisonerRoles: [],
+          active: true,
+        },
+        {
+          incidentType: 'ASSAULT',
+          incidentTypeDescription: 'Assaults 2022',
+          questionnaireId: 3,
+          questions: [
+            {
+              questionnaireQueId: 3,
+              questionSeq: 1,
+              questionDesc: 'WAS HEALTHCARE INVOLVED',
+              questionListSeq: 1,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 5,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER STAFF NAME)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: true,
+                  nextQuestionnaireQueId: 4,
+                },
+                {
+                  questionnaireAnsId: 6,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: 4,
+                },
+              ],
+            },
+            {
+              questionnaireQueId: 4,
+              questionSeq: 2,
+              questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+              questionListSeq: 2,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 7,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER DATE)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: true,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+                {
+                  questionnaireAnsId: 8,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+              ],
+            },
+          ],
+          prisonerRoles: [],
+          active: true,
+        },
+      ])
+      expect(results).toHaveLength(1)
+      expect(results[0]).toHaveProperty('message', 'There are 8 answers but 7 unique answer ids')
+    })
+
+    it('should pass if all ids & types are unique', () => {
+      const results = analyseNomisReportConfiguration([
+        {
+          incidentType: 'ASSAULT1',
+          incidentTypeDescription: 'Assaults 2021',
+          questionnaireId: 2,
+          questions: [
+            {
+              questionnaireQueId: 1,
+              questionSeq: 1,
+              questionDesc: 'WAS HEALTHCARE INVOLVED',
+              questionListSeq: 1,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 1,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER STAFF NAME)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: true,
+                  nextQuestionnaireQueId: 2,
+                },
+                {
+                  questionnaireAnsId: 2,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: 2,
+                },
+              ],
+            },
+            {
+              questionnaireQueId: 2,
+              questionSeq: 2,
+              questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+              questionListSeq: 2,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 3,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER DATE)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: true,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+                {
+                  questionnaireAnsId: 4,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+              ],
+            },
+          ],
+          prisonerRoles: [],
+          active: true,
+        },
+        {
+          incidentType: 'ASSAULT',
+          incidentTypeDescription: 'Assaults 2022',
+          questionnaireId: 3,
+          questions: [
+            {
+              questionnaireQueId: 3,
+              questionSeq: 1,
+              questionDesc: 'WAS HEALTHCARE INVOLVED',
+              questionListSeq: 1,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 5,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER STAFF NAME)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: true,
+                  nextQuestionnaireQueId: 4,
+                },
+                {
+                  questionnaireAnsId: 6,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: 4,
+                },
+              ],
+            },
+            {
+              questionnaireQueId: 4,
+              questionSeq: 2,
+              questionDesc: 'WERE THE POLICE INFORMED OF THE INCIDENT',
+              questionListSeq: 2,
+              questionActiveFlag: true,
+              multipleAnswerFlag: false,
+              answers: [
+                {
+                  questionnaireAnsId: 7,
+                  answerSeq: 1,
+                  answerDesc: 'YES (ENTER DATE)',
+                  answerListSeq: 1,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: true,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+                {
+                  questionnaireAnsId: 8,
+                  answerSeq: 2,
+                  answerDesc: 'NO',
+                  answerListSeq: 2,
+                  answerActiveFlag: true,
+                  dateRequiredFlag: false,
+                  commentRequiredFlag: false,
+                  nextQuestionnaireQueId: null,
+                },
+              ],
+            },
+          ],
           prisonerRoles: [],
           active: true,
         },
--- server/reportConfiguration/analyseNomisReportConfiguration.ts
+++ server/reportConfiguration/analyseNomisReportConfiguration.ts
@@ -2,7 +2,10 @@ import type { IncidentTypeConfiguration } from '../data/prisonApi'
 
 /**
  * Check that NOMIS report configuration is logically consistent:
+ * - ensures ids are unique
  * - ensures types are unique
+ * - ensures question ids are unique
+ * - ensures answer ids are unique
  *
  * @param incidentTypes must include ALL report configuration, not just active
  */
@@ -10,9 +13,39 @@ import type { IncidentTypeConfiguration } from '../data/prisonApi'
 export function analyseNomisReportConfiguration(incidentTypes: IncidentTypeConfiguration[]): Error[] {
   const results: Error[] = []
 
-  const typeCount = new Set(incidentTypes.map(incidentType => incidentType.incidentType)).size
-  if (typeCount !== incidentTypes.length) {
-    results.push(new Error(`There are ${incidentTypes.length} configurations but ${typeCount} unique type codes`))
+  const ids = new Set<number>()
+  const types = new Set<string>()
+  let questionCount = 0
+  const questionIds = new Set<number>()
+  let answerCount = 0
+  const answerIds = new Set<number>()
+
+  incidentTypes.forEach(incidentType => {
+    ids.add(incidentType.questionnaireId)
+    types.add(incidentType.incidentType)
+    questionCount += incidentType.questions.length
+
+    incidentType.questions.forEach(question => {
+      questionIds.add(question.questionnaireQueId)
+      answerCount += question.answers.length
+
+      question.answers.forEach(answer => {
+        answerIds.add(answer.questionnaireAnsId)
+      })
+    })
+  })
+
+  if (ids.size !== incidentTypes.length) {
+    results.push(new Error(`There are ${incidentTypes.length} configurations but ${ids.size} unique ids`))
+  }
+  if (types.size !== incidentTypes.length) {
+    results.push(new Error(`There are ${incidentTypes.length} configurations but ${types.size} unique types`))
+  }
+  if (questionIds.size !== questionCount) {
+    results.push(new Error(`There are ${questionCount} questions but ${questionIds.size} unique question ids`))
+  }
+  if (answerIds.size !== answerCount) {
+    results.push(new Error(`There are ${answerCount} answers but ${answerIds.size} unique answer ids`))
   }
 
   return results

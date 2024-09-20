aeeb17ac9066006b61b35a47e5eb1f03fccea6e9
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-09-20 18:25:23 +0100

    IR-505: Present inactive questions/answers differently
    Inactive questions/answers are greyed out. It's now possible to see
    inactive paths at a glance.
    
    This is very useful when checking questionnaires for possible errors.
    
    For example (see `RELEASED_IN_ERROR`) in a report type config some answers may lead to an inactive
    question and peraphs there is another active question with the same label.
    Was the answer supposed to lead to the equivalent active question instead?

--- server/data/incidentTypeConfiguration/conversion.ts
+++ server/data/incidentTypeConfiguration/conversion.ts
@@ -86,6 +86,7 @@ export function toTypescript({
  * @returns string containing the DOT format output
  */
 export function toGraphviz(config: DpsIncidentTypeConfiguration): string {
+  const inactiveStyle = ', style="filled", color="#DDD"'
   const questions = Object.values(config.questions)
 
   let result = `digraph ${config.incidentType} {`
@@ -101,11 +102,15 @@ export function toGraphviz(config: DpsIncidentTypeConfiguration): string {
 
   // Adds the questions as nodes, answers as edges
   for (const question of questions) {
-    result += `  ${question.id} [label = "${question.label}"];\n`
+    const questionStyle = question.active === true ? '' : inactiveStyle
+
+    result += `  ${question.id} [label = "${question.label}"${questionStyle}];\n`
     for (const answer of question.answers) {
+      const answerStyle = answer.active === true ? '' : inactiveStyle
+
       const nextNode = answer.nextQuestionId ?? 'END_NODE'
 
-      result += `  ${question.id} -> ${nextNode} [label = "${answer.label}"];\n`
+      result += `  ${question.id} -> ${nextNode} [label = "${answer.label}"${answerStyle}];\n`
     }
   }
 

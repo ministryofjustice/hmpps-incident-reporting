ca546f2100ca820533fe95eeef9d2188c8d0174a
ushkarev <ushkarev@users.noreply.github.com>
2024-10-01 09:48:22 +0100

    IR-505: Add script to validate existing report config (#199)

--- scripts/listDownloadLinks.ts
+++ scripts/listDownloadLinks.ts
@@ -1,15 +1,9 @@
 #!/usr/bin/env npx tsx
-import fs from 'node:fs'
 import path from 'node:path'
 
-import { parse as parseYaml } from 'yaml'
+import { getHelmEnvironments, type HelmEnvironment, printText } from './utils'
 
 interface Arguments {
-  baseUrl: string
-}
-
-interface Environment {
-  environment: string
   baseUrl: string
 }
 
@@ -38,67 +32,49 @@ function main() {
     'prisoner-involvement-outcome',
   ]
 
-  process.stderr.write('DPS configuration JSON downloads:\n')
+  printText('DPS configuration JSON downloads:')
   dpsTypes.forEach(type => {
-    process.stderr.write(`  - ${baseUrl}/download-report-config/dps/${type}.json\n`)
+    printText(`  - ${baseUrl}/download-report-config/dps/${type}.json`)
   })
 
-  process.stderr.write('\nNOMIS configuration JSON downloads:\n')
+  printText('\nNOMIS configuration JSON downloads:')
   nomisTypes.forEach(urlSlug => {
-    process.stderr.write(`  - ${baseUrl}/download-report-config/nomis/${urlSlug}.json\n`)
+    printText(`  - ${baseUrl}/download-report-config/nomis/${urlSlug}.json`)
   })
-  process.stderr.write('Where <type> is the NOMIS incident report type code.\n')
+  printText('Where <type> is the NOMIS incident report type code.')
 
-  process.stderr.write('\nNOMIS configuration CSV downloads:\n')
+  printText('\nNOMIS configuration CSV downloads:')
   nomisTypes.forEach(urlSlug => {
-    process.stderr.write(`  - ${baseUrl}/download-report-config/nomis/${urlSlug}.csv\n`)
+    printText(`  - ${baseUrl}/download-report-config/nomis/${urlSlug}.csv`)
   })
-  process.stderr.write('Where <type> is the NOMIS incident report type code.\n')
+  printText('Where <type> is the NOMIS incident report type code.')
 }
 
 function parseArgs(): Arguments {
   const [, fullPath, env] = process.argv
   const scriptName = `./scripts/${path.basename(fullPath)}`
-  const environments = getEnvironments()
+  const environments = getHelmEnvironments()
 
   if (!env) {
-    printHelp(scriptName, environments)
+    printHelpAndExit(scriptName, environments)
   }
 
   const environment = environments.find(e => e.environment === env)
   if (!environment) {
-    printHelp(scriptName, environments)
+    printHelpAndExit(scriptName, environments)
   }
 
   return { baseUrl: environment.baseUrl }
 }
 
-function getEnvironments(): Environment[] {
-  const helmRootPath = path.resolve(__dirname, '../helm_deploy')
-
-  return fs
-    .readdirSync(helmRootPath, { encoding: 'utf8' })
-    .filter(p => p.startsWith('values-') && p.endsWith('.yaml'))
-    .map(p => {
-      const helmValuesPath = path.join(helmRootPath, p)
-      const helmValues: { 'generic-service': { ingress: { host: string } } } = parseYaml(
-        fs.readFileSync(helmValuesPath, { encoding: 'utf8' }),
-      )
-
-      const { environment } = /^values-(?<environment>.*?).yaml$/.exec(p).groups
-      const baseUrl = `https://${helmValues['generic-service'].ingress.host}`
-      return { environment, baseUrl }
-    })
-}
-
-function printHelp(scriptName: string, environments: Environment[]): never {
+function printHelpAndExit(scriptName: string, environments: HelmEnvironment[]): never {
   const help = `
 Prints download links for DPS and NOMIS configuration:
 Usage:
   ${scriptName} <env>
 
 Where <env> is one of ${environments.map(e => e.environment).join(', ')}
-`.trim()
-  process.stderr.write(`${help}\n`)
+`
+  printText(help.trim())
   process.exit(1)
 }

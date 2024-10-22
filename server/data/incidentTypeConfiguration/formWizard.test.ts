ac9742c10631aa8677ad7e832d8a80d53e1923f7
Aldo Giambelluca <xoen@users.noreply.github.com>
2024-10-22 11:01:48 +0100

    IR-519: Various improvements to questions steps/fields (#234)
    * Fix date conditional field JS error
    
    `leadingZeros` is a boolean, not a string!
    
    * On error, user choices are retained for checkboxes
    
    * Steps don't include fields for inactive answers
    
    This shouldn't be harmful but it's also unnecessary.
    In practice it shouldn't make much difference in
    actice report type config with very few inactive answer -
    nevertheless it's best to ignore inactive answers.
    
    * Add conditional fields only for active answers
    
    * Don't generate fields for inactive questions
    
    This shouldn't be harmful but it's wasterful nevertheless.
    
    * Validate dates in conditional fields
    
    * Introduced `NextCondition` type
    
    This is the same list of valid values previously
    inlined in the `NextStep` type and in theory it shouldn't
    make any difference.
    
    However I found that TS seemed to struggle to accept
    a list of these coming out of `Array.prototype.map()`
    unless I specified its type. I could have specified
    `const next: FormWizard.NextStep` but this is an
    array of conditions really so use the singular word
    "NextStep" could be misleading.
    
    * Removed redundant string from `NextStepCondition` type
    
    This is `string |` is already mentioned in `NextStep`
    and judjing from the wording in Form Wizard docs is
    not technically a "condition" (it just goes to the given
    path unconditionally):
    
    > The next step for each step can be a relative path,
    > an external URL, or an array of conditional next steps.
    
    * Generated steps work with multiple values
    
    When a question is allowed to have multiple values
    submitting the form wasn't going to the next page.
    
    This was because a field with `multiple: true` would
    be submitted as `string[]`. This is correct but the
    naive next step condition would still try to match
    this using the default `===` op which in this case
    doesn't make sense:
    
    e.g. user submitted `['dog', 'fox']` but the value in
    the condition is `'dog'`, so `'dog' !== ['dog', 'fox']`.
    
    In order for the next step work the condition has to
    check whether any of the submitted values matches
    the condition's value:
    
    For example:
    
    ```
    // Pseudo-code
    [
      { value: 'cat', next: 'q2', op: '???' },
      { value: 'dog', next: 'q2', op: '???' },
      { value: 'fox', next: 'q2', op: '???' },
    ]
    
    valuesSubmitted = ['dog', 'cat']
    ```
    
    in this case we want Form Wizard to check if `'cat'`
    is included in `['dog', 'cat']`, if it is that's a
    match and user would be directed to `q2`.
    
    This is done using function to check submitted values
    when field has `multiple: true` (otherwise usual
    `===` operation applies).
    
    * Added tests for `generateSteps()`/`generateFields()`
    
    * Added tests for `checkMultipleValues()`
    
    * Removed now obsolete TODO comment

---
+++ server/data/incidentTypeConfiguration/formWizard.test.ts
@@ -0,0 +1,407 @@
+import { type FormWizard } from 'hmpo-form-wizard'
+import QuestionsController from '../../controllers/wip/questionsController'
+import { checkMultipleValues, generateFields, generateSteps } from './formWizard'
+import { type IncidentTypeConfiguration } from './types'
+
+const testConfig: IncidentTypeConfiguration = {
+  incidentType: 'MISCELLANEOUS',
+  active: true,
+  prisonerRoles: [],
+  startingQuestionId: 'qanimals',
+  questions: {
+    qanimals: {
+      id: 'qanimals',
+      code: 'WHICH ANIMALS DO YOU LIKE',
+      label: 'Which animals do you like?',
+      active: true,
+      multipleAnswers: true,
+      answers: [
+        {
+          id: 'qanimals-a1',
+          code: 'DOG',
+          label: 'Dog',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: 'qdog',
+        },
+        {
+          id: 'qanimals-a2',
+          code: 'CAT',
+          label: 'Cat',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: 'icecream',
+        },
+        {
+          id: 'qanimals-a3',
+          code: 'FOX',
+          label: 'Fox',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: 'icecream',
+        },
+        // Inactive answer
+        {
+          id: 'qanimals-a4',
+          code: 'HONEY BADGER',
+          label: 'Honey badger',
+          active: false,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: 'icecream',
+        },
+      ],
+    },
+    qdog: {
+      id: 'qdog',
+      code: 'DO YOU HAVE A DOG',
+      label: 'Do you have a dog?',
+      active: true,
+      multipleAnswers: false,
+      answers: [
+        {
+          id: 'qdog-a1',
+          code: 'YES',
+          label: 'Yes',
+          active: true,
+          dateRequired: true,
+          commentRequired: true,
+          nextQuestionId: 'qicecream',
+        },
+        {
+          id: 'qdog-a2',
+          code: 'NO',
+          label: 'No',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: 'qicecream',
+        },
+      ],
+    },
+    qicecream: {
+      id: 'qicecream',
+      code: 'DO YOU LIKE ICE CREAM',
+      label: 'Do you like ice cream?',
+      active: true,
+      multipleAnswers: false,
+      answers: [
+        {
+          id: 'qicecream-a1',
+          code: 'YES (SPECIFY FAVOURITE FLAVOUR)',
+          label: 'Yes (specify favourite flabour)',
+          active: true,
+          dateRequired: false,
+          commentRequired: true,
+          nextQuestionId: 'qend',
+        },
+        {
+          id: 'q2-a2',
+          code: 'no',
+          label: 'No',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: 'qend',
+        },
+      ],
+    },
+    qend: {
+      id: 'qend',
+      code: 'DO YOU WANT TO FINISH',
+      label: 'Do you want to finish?',
+      active: true,
+      multipleAnswers: false,
+      answers: [
+        {
+          id: 'q2-a1',
+          code: 'yes',
+          label: 'Yes',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: null,
+        },
+        {
+          id: 'q2-a2',
+          code: 'no',
+          label: 'No',
+          active: true,
+          dateRequired: false,
+          commentRequired: false,
+          nextQuestionId: null,
+        },
+      ],
+    },
+    // Inactive question
+    'q3-inactive': {
+      id: 'q3-inactive',
+      code: 'REMOVE',
+      label: 'Remove this question please',
+      active: false,
+      multipleAnswers: false,
+      answers: [],
+    },
+  },
+}
+
+describe('generateSteps()', () => {
+  it('returns list of steps for the given config', () => {
+    const steps = generateSteps(testConfig)
+
+    expect(steps).toEqual({
+      '/': {
+        entryPoint: true,
+        reset: true,
+        resetJourney: true,
+        skip: true,
+        next: 'qanimals',
+      },
+      '/qanimals': {
+        controller: QuestionsController,
+        fields: ['qanimals'],
+        template: 'questionPage',
+        next: [
+          {
+            field: 'qanimals',
+            value: 'DOG',
+            op: checkMultipleValues,
+            next: 'qdog',
+          },
+          {
+            field: 'qanimals',
+            value: 'CAT',
+            op: checkMultipleValues,
+            next: 'icecream',
+          },
+          {
+            field: 'qanimals',
+            value: 'FOX',
+            op: checkMultipleValues,
+            next: 'icecream',
+          },
+        ],
+      },
+      '/qdog': {
+        controller: QuestionsController,
+        fields: ['qdog', 'qdog-qdog-a1-date', 'qdog-qdog-a1-comment'],
+        template: 'questionPage',
+        next: [
+          {
+            field: 'qdog',
+            op: '===',
+            value: 'YES',
+            next: 'qicecream',
+          },
+          {
+            field: 'qdog',
+            op: '===',
+            value: 'NO',
+            next: 'qicecream',
+          },
+        ],
+      },
+      '/qicecream': {
+        controller: QuestionsController,
+        fields: ['qicecream', 'qicecream-qicecream-a1-comment'],
+        template: 'questionPage',
+        next: [
+          {
+            field: 'qicecream',
+            op: '===',
+            value: 'YES (SPECIFY FAVOURITE FLAVOUR)',
+            next: 'qend',
+          },
+          {
+            field: 'qicecream',
+            op: '===',
+            value: 'no',
+            next: 'qend',
+          },
+        ],
+      },
+      '/qend': {
+        controller: QuestionsController,
+        fields: ['qend'],
+        template: 'questionPage',
+        next: [
+          {
+            field: 'qend',
+            op: '===',
+            value: 'yes',
+            next: null,
+          },
+          {
+            field: 'qend',
+            op: '===',
+            value: 'no',
+            next: null,
+          },
+        ],
+      },
+    })
+  })
+})
+
+describe('generateFields()', () => {
+  it('returns list of fields for the given config', () => {
+    const fields = generateFields(testConfig)
+
+    expect(fields).toEqual({
+      qanimals: {
+        name: 'qanimals',
+        label: 'Which animals do you like?',
+        validate: ['required'],
+        multiple: true,
+        component: 'govukCheckboxes',
+        items: [
+          {
+            id: 'qanimals-a1',
+            name: 'qanimals-a1',
+            value: 'DOG',
+            label: 'Dog',
+            dateRequired: false,
+            commentRequired: false,
+          },
+          {
+            id: 'qanimals-a2',
+            name: 'qanimals-a2',
+            value: 'CAT',
+            label: 'Cat',
+            dateRequired: false,
+            commentRequired: false,
+          },
+          {
+            id: 'qanimals-a3',
+            name: 'qanimals-a3',
+            value: 'FOX',
+            label: 'Fox',
+            dateRequired: false,
+            commentRequired: false,
+          },
+        ],
+      },
+      qdog: {
+        name: 'qdog',
+        label: 'Do you have a dog?',
+        validate: ['required'],
+        multiple: false,
+        component: 'govukRadios',
+        items: [
+          {
+            id: 'qdog-a1',
+            name: 'qdog-a1',
+            value: 'YES',
+            label: 'Yes',
+            dateRequired: true,
+            commentRequired: true,
+          },
+          {
+            id: 'qdog-a2',
+            name: 'qdog-a2',
+            value: 'NO',
+            label: 'No',
+            dateRequired: false,
+            commentRequired: false,
+          },
+        ],
+      },
+      'qdog-qdog-a1-date': {
+        name: 'qdog-qdog-a1-date',
+        label: 'Date',
+        component: 'mojDatePicker',
+        validate: ['required', 'ukDate'],
+        dependent: {
+          field: 'qdog',
+          value: 'YES',
+        },
+      },
+      'qdog-qdog-a1-comment': {
+        name: 'qdog-qdog-a1-comment',
+        label: 'Comment',
+        component: 'govukInput',
+        validate: ['required'],
+        dependent: {
+          field: 'qdog',
+          value: 'YES',
+        },
+      },
+      qicecream: {
+        name: 'qicecream',
+        label: 'Do you like ice cream?',
+        validate: ['required'],
+        multiple: false,
+        component: 'govukRadios',
+        items: [
+          {
+            id: 'qicecream-a1',
+            name: 'qicecream-a1',
+            value: 'YES (SPECIFY FAVOURITE FLAVOUR)',
+            label: 'Yes (specify favourite flabour)',
+            dateRequired: false,
+            commentRequired: true,
+          },
+          {
+            id: 'q2-a2',
+            name: 'q2-a2',
+            value: 'no',
+            label: 'No',
+            dateRequired: false,
+            commentRequired: false,
+          },
+        ],
+      },
+      'qicecream-qicecream-a1-comment': {
+        name: 'qicecream-qicecream-a1-comment',
+        label: 'Comment',
+        component: 'govukInput',
+        validate: ['required'],
+        dependent: {
+          field: 'qicecream',
+          value: 'YES (SPECIFY FAVOURITE FLAVOUR)',
+        },
+      },
+      qend: {
+        name: 'qend',
+        label: 'Do you want to finish?',
+        validate: ['required'],
+        multiple: false,
+        component: 'govukRadios',
+        items: [
+          {
+            id: 'q2-a1',
+            name: 'q2-a1',
+            value: 'yes',
+            label: 'Yes',
+            dateRequired: false,
+            commentRequired: false,
+          },
+          {
+            id: 'q2-a2',
+            name: 'q2-a2',
+            value: 'no',
+            label: 'No',
+            dateRequired: false,
+            commentRequired: false,
+          },
+        ],
+      },
+    })
+  })
+})
+
+describe('checkMultipleValues()', () => {
+  it.each([
+    { desc: 'no values submitted yet', submittedValues: undefined, expected: false },
+    { desc: 'submitted values do not match', submittedValues: ['cat', 'lizard'], expected: false },
+    { desc: 'submitted values match', submittedValues: ['fox', 'dog'], expected: true },
+  ])('returns $expected when $desc', ({ submittedValues, expected }) => {
+    const condition = { value: 'dog' }
+
+    const result = checkMultipleValues(submittedValues, null as FormWizard.Request, null as Express.Response, condition)
+    expect(result).toEqual(expected)
+  })
+})

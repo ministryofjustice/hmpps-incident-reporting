7b0f4b4c0ea94b138ac3c5b52a70af82dea6fa65
Josh Jones <josh.jones4@digital.justice.gov.uk>
2024-10-23 13:13:13 +0100

    added filters with placeholder for reporting officer until api call is updated

--- server/routes/debug.ts
+++ server/routes/debug.ts
@@ -6,11 +6,15 @@ import type { GovukErrorSummaryItem } from '../utils/govukFrontend'
 import { pagination } from '../utils/pagination'
 import { parseDateInput } from '../utils/utils'
 import type { Services } from '../services'
+import { type Type, types, type Status, statuses } from '../reportConfiguration/constants'
 
 interface ListFormData {
   prisonId?: string
   fromDate?: string
   toDate?: string
+  incidentType?: Type
+  reportingOfficer?: string
+  incidentStatuses?: Status
   page?: string
 }
 
@@ -21,11 +25,22 @@ export default function makeDebugRoutes(services: Services): Record<string, Requ
     async eventList(req, res) {
       const { incidentReportingApi, prisonApi } = res.locals.apis
 
-      const { prisonId, fromDate: fromDateInput, toDate: toDateInput, page }: ListFormData = req.query
+      const {
+        prisonId,
+        fromDate: fromDateInput,
+        toDate: toDateInput,
+        incidentType,
+        reportingOfficer,
+        page,
+        incidentStatuses,
+      }: ListFormData = req.query
       const formValues: ListFormData = {
         prisonId,
         fromDate: fromDateInput,
         toDate: toDateInput,
+        incidentType: incidentType as Type,
+        reportingOfficer,
+        incidentStatuses: incidentStatuses as Status,
         page,
       }
 
@@ -62,6 +77,8 @@ export default function makeDebugRoutes(services: Services): Record<string, Requ
         prisonId,
         incidentDateFrom: fromDate,
         incidentDateUntil: toDate,
+        type: incidentType,
+        status: incidentStatuses,
         page: pageNumber - 1,
         // sort: ['eventDateAndTime,ASC'],
       })
@@ -76,6 +93,12 @@ export default function makeDebugRoutes(services: Services): Record<string, Requ
       if (toDateInput) {
         queryString.append('toDate', toDateInput)
       }
+      if (incidentType) {
+        queryString.append('incidentType', incidentType)
+      }
+      if (incidentStatuses) {
+        queryString.append('incidentStatus', incidentStatuses)
+      }
 
       const urlPrefix = `/incidents?${queryString}&`
       const paginationParams = pagination(
@@ -86,21 +109,36 @@ export default function makeDebugRoutes(services: Services): Record<string, Requ
         reportsResponse.totalElements,
         reportsResponse.size,
       )
-      const noFiltersSupplied = Boolean(!prisonId && !fromDate && !toDate)
+      const noFiltersSupplied = Boolean(!prisonId && !fromDate && !toDate && !incidentType && !incidentStatuses)
 
       const reports = reportsResponse.content
       const usernames = reports.map(report => report.reportedBy)
       const usersLookup = await userService.getUsers(res.locals.systemToken, usernames)
       const prisonsLookup = await prisonApi.getPrisons()
+      const reportingOfficers = Object.values(usersLookup).map(user => ({
+        value: user.username,
+        text: user.name,
+      }))
       const prisons = Object.values(prisonsLookup).map(prison => ({
         value: prison.agencyId,
         text: prison.description,
+      }))
+      const incidentTypes = Object.values(types).map(incType => ({
+        value: incType.code,
+        text: incType.description,
+      }))
+      const statusItems = Object.values(statuses).map(status => ({
+        value: status.code,
+        text: status.description,
       }))
 
       res.render('pages/debug/eventList', {
         reports,
         prisons,
         usersLookup,
+        reportingOfficers,
+        incidentTypes,
+        statusItems,
         formValues,
         errors,
         todayAsShortDate,

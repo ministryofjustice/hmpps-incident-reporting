fe5aed05e414daa102813535c63b0654feb6673e
Aldo Giambelluca <xoen@users.noreply.github.com>
2024-10-24 10:58:47 +0100

    IR-519: Improve next step conditions (#241)
    * Doc comments for `generateSteps()`/`generateFields()`
    
    and remove TODO comments
    
    * Improved `NextStepCondition` type
    
    - value in the condition could be a `string` or a `string[]`
    - extracted operator function type and allow `op` to be one of the
      build-in operations or a function
    
    TS was complaining when building `NextStepCondition[]` with a mixture
    of `op: 'in'` and `op: () => { ... }`.
    
    * Generated steps now group answers
    
    Answers leading to the same next question would be grouped
    together in the same next step condition.
    
    This is more legible and possibly more compact.
    Potentially for questions with a lot of answers
    it means the `checkMultipleValues()` function (or `===`)
    would be invoked less times.
    
    It also doesn't harm that the format is more legible
    and easier to debug.
    
    * Tidied question page debugging section

--- server/data/incidentTypeConfiguration/formWizard.test.ts
+++ server/data/incidentTypeConfiguration/formWizard.test.ts
@@ -167,19 +167,13 @@ describe('generateSteps()', () => {
         next: [
           {
             field: 'qanimals',
-            value: 'DOG',
+            value: ['DOG'],
             op: checkMultipleValues,
             next: 'qdog',
           },
           {
             field: 'qanimals',
-            value: 'CAT',
-            op: checkMultipleValues,
-            next: 'icecream',
-          },
-          {
-            field: 'qanimals',
-            value: 'FOX',
+            value: ['CAT', 'FOX'],
             op: checkMultipleValues,
             next: 'icecream',
           },
@@ -192,14 +186,8 @@ describe('generateSteps()', () => {
         next: [
           {
             field: 'qdog',
-            op: '===',
-            value: 'YES',
-            next: 'qicecream',
-          },
-          {
-            field: 'qdog',
-            op: '===',
-            value: 'NO',
+            op: 'in',
+            value: ['YES', 'NO'],
             next: 'qicecream',
           },
         ],
@@ -211,14 +199,8 @@ describe('generateSteps()', () => {
         next: [
           {
             field: 'qicecream',
-            op: '===',
-            value: 'YES (SPECIFY FAVOURITE FLAVOUR)',
-            next: 'qend',
-          },
-          {
-            field: 'qicecream',
-            op: '===',
-            value: 'no',
+            op: 'in',
+            value: ['YES (SPECIFY FAVOURITE FLAVOUR)', 'no'],
             next: 'qend',
           },
         ],
@@ -230,14 +212,8 @@ describe('generateSteps()', () => {
         next: [
           {
             field: 'qend',
-            op: '===',
-            value: 'yes',
-            next: null,
-          },
-          {
-            field: 'qend',
-            op: '===',
-            value: 'no',
+            op: 'in',
+            value: ['yes', 'no'],
             next: null,
           },
         ],
@@ -381,7 +357,7 @@ describe('checkMultipleValues()', () => {
     { desc: 'submitted values do not match', submittedValues: ['cat', 'lizard'], expected: false },
     { desc: 'submitted values match', submittedValues: ['fox', 'dog'], expected: true },
   ])('returns $expected when $desc', ({ submittedValues, expected }) => {
-    const condition = { value: 'dog' }
+    const condition = { value: ['dog', 'turtle'] }
 
     const result = checkMultipleValues(submittedValues, null as FormWizard.Request, null as Express.Response, condition)
     expect(result).toEqual(expected)

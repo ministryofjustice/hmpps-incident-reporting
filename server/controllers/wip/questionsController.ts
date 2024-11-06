54527d6c533a3218d0f9f9df1d05487b5c3ce6ec
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-11-06 16:05:15 +0000

    Set back link in questions page

--- server/controllers/wip/questionsController.ts
+++ server/controllers/wip/questionsController.ts
@@ -1,3 +1,10 @@
+import { FormWizard } from 'hmpo-form-wizard'
+import type express from 'express'
 import { BaseController } from '../index'
 
-export default class QuestionsController extends BaseController {}
+export default class QuestionsController extends BaseController {
+  getBackLink(_req: FormWizard.Request, _res: express.Response): string {
+    // TODO: Change to `/reports/` page once we have it
+    return '/incidents/'
+  }
+}

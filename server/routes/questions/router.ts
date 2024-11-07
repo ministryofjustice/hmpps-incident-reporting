ca2dfd9919104d7bee44cc94132cf9d2c55264c0
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-11-07 16:36:24 +0000

    Questions router: Set `mergeParams = true`
    This is necessary in order for middleware to be able
    to read things like params (how the report ID is provided)

--- server/routes/questions/router.ts
+++ server/routes/questions/router.ts
@@ -26,14 +26,20 @@ router.use(
     const steps = generateSteps(config)
     const fields = generateFields(config)
 
-    return wizard(steps, fields, {
-      name: `${reportType}-questions`,
+    const wizardRouter = wizard(steps, fields, {
+      name: `${reportId}-questions`,
       templatePath: 'pages/wip/questions',
       // Needs to be false, session already handled by application
       checkSession: false,
       // Needs to be false, CSRF already handled by application
       csrf: false,
-    })(req, res, next)
+    })
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore because express types do not mention this property
+    // and form wizard does not allow you to pass in config for it's
+    // root router
+    wizardRouter.mergeParams = true
+    wizardRouter(req, res, next)
   }),
 )
 

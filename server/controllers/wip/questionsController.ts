89399543c0f64cfccf00e1925a8597d49e95d519
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2024-11-08 16:53:35 +0000

    Populate form answers from API Report
    Populate the questions form fields with values coming from the API report.
    Fields are populated only when the user didn't already set these values,
    e.g. user-submitted choices are retained.
    
    Some mapping logic is needed to determine the various field names
    from the Report API as these don't contain the answer IDs (the data coming
    from the API has the list of `responses` and the `response` field is
    the answer "code", e.g. the text which is used as an identifier).
    
    Any given question may have multiple responses.
    Each Response may also require comment and/or date.
    
    For example a question 'question-1' may allow for multiple answers. Then
    each answer may also optionally require a date and/or a comment, for example
    `answer-A` may require both, these fields values (`question-1-answer-A-date`
    and `question-1-answer-A-comment`) would be read from the `response`'s
    `responseDate` and `additionalInformation` fields respectively.
    
    But the important point is that several responses may need these, e.g.
    
    Question 1
    - [X] Answer A
      - Date:    [ 30/12/2024 ]
      - Comment: [ Answer A's comment ]
    - [X] Answer B
      - Date:    [ 31/12/2024 ]
      - Comment: [ Answer B's comment ]

--- server/controllers/wip/questionsController.ts
+++ server/controllers/wip/questionsController.ts
@@ -1,6 +1,15 @@
 import { FormWizard } from 'hmpo-form-wizard'
 import type express from 'express'
 import { BaseController } from '../index'
+import { ReportWithDetails } from '../../data/incidentReportingApi'
+import format from '../../utils/format'
+import { getIncidentTypeConfiguration } from '../../reportConfiguration/types'
+import {
+  AnswerConfiguration,
+  IncidentTypeConfiguration,
+  QuestionConfiguration,
+} from '../../data/incidentTypeConfiguration/types'
+import logger from '../../../logger'
 
 export default class QuestionsController extends BaseController<FormWizard.MultiValues> {
   getBackLink(_req: FormWizard.Request, _res: express.Response): string {
@@ -18,6 +27,86 @@ export default class QuestionsController extends BaseController<FormWizard.Multi
     const reportId = req.params.id
 
     res.locals.report = await incidentReportingApi.getReportWithDetailsById(reportId)
+    res.locals.reportConfig = await getIncidentTypeConfiguration(res.locals.report.type)
     next()
+  }
+
+  getValues(
+    req: FormWizard.Request<FormWizard.MultiValues, string>,
+    res: express.Response,
+    callback: FormWizard.Callback<FormWizard.MultiValues>,
+  ) {
+    return super.getValues(req, res, async (err, values) => {
+      if (err) {
+        return callback(err)
+      }
+
+      const formValues = { ...values }
+
+      const report = res.locals.report as ReportWithDetails
+      const reportConfig = res.locals.reportConfig as IncidentTypeConfiguration
+
+      for (const question of report.questions) {
+        const fieldName = this.questionIdFromCode(question.code)
+        const questionConfig: QuestionConfiguration = reportConfig.questions[fieldName]
+        if (questionConfig === undefined) {
+          logger.warn(new Error(`Question with code '${fieldName}' not found in ${report.type}'s configuration.`))
+          // eslint-disable-next-line no-continue
+          continue
+        }
+
+        // Top-level answer(s)
+        if (formValues[fieldName] === undefined) {
+          const reportValues = question.responses.map(response => response.response)
+          formValues[fieldName] = questionConfig.multipleAnswers ? reportValues : reportValues[0]
+        }
+
+        // Populate comment/date fields for each response
+        // NOTE: Each response may have its own associated comment and/or date
+        for (const response of question.responses) {
+          const answerCode = response.response
+          const answerConfig = this.findAnswerConfigByCode(answerCode, questionConfig)
+          if (answerConfig === undefined) {
+            logger.warn(
+              new Error(
+                `answer with code '${answerCode}' not found in ${report.type}'s question '${questionConfig.id}' configuration.`,
+              ),
+            )
+            // eslint-disable-next-line no-continue
+            continue
+          }
+
+          // comment field
+          if (answerConfig.commentRequired) {
+            const commentFieldName = `${questionConfig.id}-${answerConfig.id}-comment`
+            if (formValues[commentFieldName] === undefined) {
+              formValues[commentFieldName] = response.additionalInformation
+            }
+          }
+
+          // date field
+          if (answerConfig.dateRequired) {
+            const dateFieldName = `${questionConfig.id}-${answerConfig.id}-date`
+            if (formValues[dateFieldName] === undefined) {
+              formValues[dateFieldName] = format.shortDate(response.responseDate)
+            }
+          }
+        }
+      }
+
+      return callback(null, formValues)
+    })
+  }
+
+  /** Finds the Answer config for a given the answer code */
+  private findAnswerConfigByCode(answerCode: string, questionConfig: QuestionConfiguration): AnswerConfiguration {
+    return questionConfig.answers.find(answerConfig => answerConfig.code.trim() === answerCode.trim())
+  }
+
+  /** Strips `QID-0...` prefix and returns the question ID */
+  private questionIdFromCode(code: string): string {
+    const re = /^QID-0*/
+
+    return code.replace(re, '')
   }
 }

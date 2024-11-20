eb2c129e74c7fa802a26ffb9013a76dcca0602dc
ushkarev <ushkarev@users.noreply.github.com>
2024-11-20 10:33:16 +0000

    IR-68: Sync upstream changes to template (#273)
    Replace csurf with csrf-sync (hmpps-template-typesript#481)
    
    Co-authored-by: James Reed <109068236+ReedSoftware@users.noreply.github.com>

--- server/middleware/setUpCsrf.ts
+++ server/middleware/setUpCsrf.ts
@@ -1,4 +1,4 @@
-import csurf from 'csurf'
+import { csrfSync } from 'csrf-sync'
 import { Router } from 'express'
 
 const testMode = process.env.NODE_ENV === 'test'
@@ -8,7 +8,18 @@ export default function setUpCsrf(): Router {
 
   // CSRF protection
   if (!testMode) {
-    router.use(csurf())
+    const {
+      // This is the default CSRF protection middleware
+      csrfSynchronisedProtection,
+    } = csrfSync({
+      // By default, csrf-sync uses x-csrf-token header, but we use the token in forms and send it in the request body, so change getTokenFromRequest so it grabs from there
+      getTokenFromRequest: req => {
+        // eslint-disable-next-line no-underscore-dangle
+        return req.body._csrf
+      },
+    })
+
+    router.use(csrfSynchronisedProtection)
   }
 
   router.use((req, res, next) => {

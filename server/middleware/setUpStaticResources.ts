eb3a76375371a869a6cf466dfa2ef93172d71a0c
Josh Jones <josh.jones4@digital.justice.gov.uk>
2025-01-23 17:34:51 +0000

    Added accessible autocomplete component configured to be a usable macro in nunjucks templates. Applied component to establishment and incident type on dashboard

--- server/middleware/setUpStaticResources.ts
+++ server/middleware/setUpStaticResources.ts
@@ -21,6 +21,7 @@ export default function setUpStaticResources(): Router {
     '/node_modules/govuk-frontend/dist',
     '/node_modules/@ministryofjustice/frontend/moj/assets',
     '/node_modules/@ministryofjustice/frontend',
+    '/node_modules/accessible-autocomplete/dist',
   ).forEach(dir => {
     router.use('/assets', express.static(path.join(process.cwd(), dir), staticResourcesConfig))
   })

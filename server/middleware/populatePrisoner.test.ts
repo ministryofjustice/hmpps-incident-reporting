a5533bce344ebdf28bfe83117153cb9e18996188
Igor <igor.ushkarev@digital.justice.gov.uk>
2025-02-18 14:41:53 +0000

    Make object-lookup middleware more resilient to missing route parameters

--- server/middleware/populatePrisoner.test.ts
+++ server/middleware/populatePrisoner.test.ts
@@ -41,4 +41,19 @@ describe('prisoner-loading middleware', () => {
     expect(next).toHaveBeenCalledWith(expect.objectContaining({ message: 'Not Found', status: 404 }))
     expect(res.locals.prisoner).toBeUndefined()
   })
+
+  it('should fail if prisoner number parameter is not supplied', async () => {
+    const getPrisoner = jest.fn()
+
+    const req = { params: {} } as unknown as Request
+    const res = { locals: { apis: { offenderSearchApi: { getPrisoner } } } } as unknown as Response
+    const next: NextFunction = jest.fn()
+    await populatePrisoner()(req, res, next)
+
+    expect(getPrisoner).not.toHaveBeenCalled()
+    expect(res.locals.prisoner).toBeUndefined()
+    expect(next).toHaveBeenCalledWith(
+      expect.objectContaining({ message: 'populatePrisoner() requires req.params.prisonerNumber', status: 501 }),
+    )
+  })
 })

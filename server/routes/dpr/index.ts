0620cf2a199f92af1acf32930455798856a757da
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-03-11 16:41:44 +0000

    IR-824 DPR reporting enhancements - Add callback to setup routes using system token - Add correct breadcrumbs - upgrade DPR UI library to fix date issue

--- server/routes/dpr/index.ts
+++ server/routes/dpr/index.ts
@@ -1,19 +1,20 @@
-import { Router } from 'express'
-import defaultTokenProvider from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/report-list/defaultTokenProvider'
+import { NextFunction, Request, Response, Router } from 'express'
 import ReportListUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/report-list/utils'
 import config from '../../config'
+import { IncidentReportingApi } from '../../data/incidentReportingApi'
+import { services } from '../../services'
+
+async function getDefinitions() {
+  const systemToken = await services().hmppsAuthClient.getSystemClientToken()
+  const incidentReportingApiSystem = new IncidentReportingApi(systemToken)
+  return incidentReportingApiSystem.getManagementReportDefinitions()
+}
 
 // eslint-disable-next-line import/prefer-default-export
 export function dprRouter(): Router {
   const router = Router({ mergeParams: true })
-
-  router.get('/', async (req, res) => {
-    const { incidentReportingApi } = res.locals.apis
-
-    // TODO: This should be cached and the routes setup on initialisation
-    const definitions = await incidentReportingApi.getManagementReportDefinitions()
-
-    for (const definition of definitions) {
+  getDefinitions().then(allDefinitions => {
+    for (const definition of allDefinitions) {
       for (const variant of definition.variants) {
         router.get(
           `/${definition.id}/${variant.id}`,
@@ -24,12 +25,18 @@ export function dprRouter(): Router {
             apiUrl: config.apis.hmppsIncidentReportingApi.url,
             apiTimeout: config.apis.hmppsIncidentReportingApi.timeout.deadline,
             layoutTemplate: 'partials/dprLayout.njk',
-            tokenProvider: defaultTokenProvider,
+            tokenProvider: (req: Request, res: Response, next: NextFunction) => {
+              return res.locals.systemToken
+            },
           }),
         )
       }
     }
+  })
 
+  router.get('/', async (req, res) => {
+    const { incidentReportingApi } = res.locals.apis
+    const definitions = await incidentReportingApi.getManagementReportDefinitions()
     res.render('pages/managementReporting/index.njk', { definitions })
   })
 

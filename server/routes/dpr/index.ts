0d30fb15954a9ba02c76fb056e79e3b4e0dff3c3
Michael Willis <michael.willis@digital.justice.gov.uk>
2025-03-12 13:24:00 +0000

    Initialize incident reporting definitions routes only once

--- server/routes/dpr/index.ts
+++ server/routes/dpr/index.ts
@@ -1,43 +1,41 @@
-import { NextFunction, Request, Response, Router } from 'express'
+import { Request, Response, Router } from 'express'
 import ReportListUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/report-list/utils'
 import config from '../../config'
-import { IncidentReportingApi } from '../../data/incidentReportingApi'
-import { services } from '../../services'
 
-async function getDefinitions() {
-  const systemToken = await services().hmppsAuthClient.getSystemClientToken()
-  const incidentReportingApiSystem = new IncidentReportingApi(systemToken)
-  return incidentReportingApiSystem.getManagementReportDefinitions()
-}
+let definitionsRoutesInitialised: boolean = false
 
 // eslint-disable-next-line import/prefer-default-export
 export function dprRouter(): Router {
   const router = Router({ mergeParams: true })
-  getDefinitions().then(allDefinitions => {
-    for (const definition of allDefinitions) {
-      for (const variant of definition.variants) {
-        router.get(
-          `/${definition.id}/${variant.id}`,
-          ReportListUtils.createReportListRequestHandler({
-            title: variant.name,
-            definitionName: definition.id,
-            variantName: variant.id,
-            apiUrl: config.apis.hmppsIncidentReportingApi.url,
-            apiTimeout: config.apis.hmppsIncidentReportingApi.timeout.deadline,
-            layoutTemplate: 'partials/dprLayout.njk',
-            tokenProvider: (req: Request, res: Response, next: NextFunction) => {
-              return res.locals.systemToken
-            },
-          }),
-        )
-      }
-    }
-  })
 
   router.get('/', async (req, res) => {
     const { incidentReportingApi } = res.locals.apis
-    const definitions = await incidentReportingApi.getManagementReportDefinitions()
-    res.render('pages/managementReporting/index.njk', { definitions })
+
+    if (definitionsRoutesInitialised === false) {
+      incidentReportingApi.getManagementReportDefinitions().then(allDefinitions => {
+        for (const definition of allDefinitions) {
+          for (const variant of definition.variants) {
+            router.get(
+              `/${definition.id}/${variant.id}`,
+              ReportListUtils.createReportListRequestHandler({
+                title: variant.name,
+                definitionName: definition.id,
+                variantName: variant.id,
+                apiUrl: config.apis.hmppsIncidentReportingApi.url,
+                apiTimeout: config.apis.hmppsIncidentReportingApi.timeout.deadline,
+                layoutTemplate: 'partials/dprLayout.njk',
+                tokenProvider: (request: Request, response: Response) => {
+                  return response.locals.systemToken
+                },
+              }),
+            )
+          }
+        }
+        definitionsRoutesInitialised = true
+      })
+    }
+    const userDefinitions = await incidentReportingApi.getManagementReportDefinitions()
+    res.render('pages/managementReporting/index.njk', { userDefinitions })
   })
 
   return router

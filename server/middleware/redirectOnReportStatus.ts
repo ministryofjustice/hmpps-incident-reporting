ec7c5e961594c923f1050d88e16134a3952fdb2f
Igor <igor.ushkarev@digital.justice.gov.uk>
2025-03-23 13:00:19 +0000

    Add middleware to prevent access to routes that require a specific report status

---
+++ server/middleware/redirectOnReportStatus.ts
@@ -0,0 +1,28 @@
+import type { NextFunction, Request, RequestHandler, Response } from 'express'
+import { NotImplemented } from 'http-errors'
+
+import logger from '../../logger'
+import type { Status } from '../reportConfiguration/constants'
+
+// eslint-disable-next-line import/prefer-default-export
+export function redirectIfStatusNot(...statuses: Status[]): RequestHandler {
+  if (!statuses.length) {
+    throw new NotImplemented('redirectIfStatusNot() requires at least one status')
+  }
+
+  return (_req: Request, res: Response, next: NextFunction): void => {
+    const { report } = res.locals
+    if (!report) {
+      // expect to always be used after populateReport() middleware
+      next(new NotImplemented('redirectIfStatusNot() requires res.locals.report'))
+      return
+    }
+
+    if (statuses.includes(report.status)) {
+      logger.info('Report %s status is %s but route requires %j', report.id, report.status, statuses)
+      next()
+    } else {
+      res.redirect(res.locals.reportUrl)
+    }
+  }
+}

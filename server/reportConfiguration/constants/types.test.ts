6c7aa89afe2fe1b9d7d3508d4a8acbaa2ef2aabe
Igor <igor.ushkarev@digital.justice.gov.uk>
2025-04-02 00:24:11 +0100

    Test to ensure type codes and descriptions are specified as expected

--- server/reportConfiguration/constants/types.test.ts
+++ server/reportConfiguration/constants/types.test.ts
@@ -1,4 +1,20 @@
-import { getTypeDetails } from './types'
+import { getTypeDetails, types } from './types'
+import { typeFamilies } from './typeFamilies'
+
+describe('Type family', () => {
+  it('should share the same code prefix for each type within a family', () => {
+    types.forEach(type => {
+      expect(type.code.startsWith(`${type.familyCode}_`)).toBe(true)
+    })
+  })
+
+  it('should have the same description for each type within a family', () => {
+    const typeFamilyDescriptions = Object.fromEntries(typeFamilies.map(({ code, description }) => [code, description]))
+    types.forEach(type => {
+      expect(type.description).toEqual(typeFamilyDescriptions[type.familyCode])
+    })
+  })
+})
 
 describe('getTypeDetails()', () => {
   it('should find active codes', () => {

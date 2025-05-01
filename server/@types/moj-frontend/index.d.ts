aed716ad9249e274a4e4f3bce0f4d6540dcd5706
Igor <igor.ushkarev@digital.justice.gov.uk>
2025-05-01 10:51:06 +0100

    Turn on global GDS style for DPR pages so that the table links use the right colour etc

--- server/@types/moj-frontend/index.d.ts
+++ server/@types/moj-frontend/index.d.ts
@@ -1,3 +1,9 @@
+/**
+ * MoJ Frontend types based on v5.1.0
+ * NB: this was recreated manually from javascript sources and remains incomplete!
+ * For example, no concrete component classes are included.
+ */
+
 declare module '@ministryofjustice/frontend' {
   function initAll(options?: { scope?: HTMLElement }): void
 }

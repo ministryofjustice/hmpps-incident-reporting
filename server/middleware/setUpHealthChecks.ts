5b92f01cde471127574c28c9d5c95d88f5235086
Aldo Giambelluca <xoen@users.noreply.github.com>
2025-05-14 09:44:06 +0100

    IR-1027: Backport template changes (monitoring library) (#436)
    * Moving to monitoring library (#479)
    
    * Removed now unused agentkeepalive dependency
    
    * Stub more healtcheck APIs for integration tests
    
    * Removed non-standard /info integration test
    
    We're now using the standard hmpps-monitoring which doesn't
    return non-standard `activeAgencies`. Removing that
    integration test assertion.
    
    * Revert "Removed non-standard /info integration test"
    
    This reverts commit e3eba7d96e28f3754cbe9d0e9ac3eabb465ff643.
    
    * Keep `activeAgencies` in `ApplicationInfo`
    
    [Igor said]:
    
    > this i supposed to drive the display of this service on DPS so it should not be removed
    
    [Igor said]: https://github.com/ministryofjustice/hmpps-incident-reporting/pull/436#discussion_r2087127434
    
    * Use monitoring's `additionalFields` mechanism
    
    to expose `activeAgencies`.
    
    See: https://github.com/ministryofjustice/hmpps-typescript-lib/commit/0d934885ce4067a8864d2d708c80ce7cfc09de56
    
    ---------
    
    Co-authored-by: Andrew Lee <1517745+andrewrlee@users.noreply.github.com>

--- server/middleware/setUpHealthChecks.ts
+++ server/middleware/setUpHealthChecks.ts
@@ -1,41 +1,23 @@
 import express, { type Router } from 'express'
+import { monitoringMiddleware, endpointHealthComponent } from '@ministryofjustice/hmpps-monitoring'
 
 import type { ApplicationInfo } from '../applicationInfo'
+import logger from '../../logger'
 import config from '../config'
-import healthcheck from '../services/healthCheck'
 
 export default function setUpHealthChecks(applicationInfo: ApplicationInfo): Router {
   const router = express.Router()
 
-  router.get('/health', (req, res, next): void => {
-    healthcheck(applicationInfo, result => {
-      if (result.status !== 'UP') {
-        res.status(503)
-      }
-      res.json(result)
-    })
+  const apiConfig = Object.entries(config.apis)
+
+  const middleware = monitoringMiddleware({
+    applicationInfo,
+    healthComponents: apiConfig.map(([name, options]) => endpointHealthComponent(logger, name, options)),
   })
 
-  router.get('/ping', (req, res): void => {
-    res.send({
-      status: 'UP',
-    })
-  })
-
-  router.get('/info', (req, res): void => {
-    res.json({
-      git: {
-        branch: applicationInfo.branchName,
-      },
-      build: {
-        artifact: applicationInfo.applicationName,
-        version: applicationInfo.buildNumber,
-        name: applicationInfo.applicationName,
-      },
-      productId: applicationInfo.productId,
-      activeAgencies: config.activePrisons,
-    })
-  })
+  router.get('/health', middleware.health)
+  router.get('/info', middleware.info)
+  router.get('/ping', middleware.ping)
 
   return router
 }

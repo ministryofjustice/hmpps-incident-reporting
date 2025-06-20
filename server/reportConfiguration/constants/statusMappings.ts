a03dc1e73ba500c2bb65780e5bf7449fb9f72497
Igor <igor.ushkarev1@justice.gov.uk>
2025-06-20 16:05:57 +0100

    Make work list codes strict (the type was `string` but now is `'toDo' | 'submitted' | 'done'`

--- server/reportConfiguration/constants/statusMappings.ts
+++ server/reportConfiguration/constants/statusMappings.ts
@@ -1,13 +1,7 @@
 import type { Status } from './statuses'
 
-export type WorkListMapping = {
-  code: string
-  description: string
-  statuses: Status[]
-}
-
-/** Report work list status mappings */
-export const workListMapping: WorkListMapping[] = [
+/** Report statuses grouped into work lists */
+export const workLists = [
   { code: 'toDo', description: 'To do', statuses: ['DRAFT', 'NEEDS_UPDATING', 'REOPENED'] },
 
   {
@@ -18,21 +12,19 @@ export const workListMapping: WorkListMapping[] = [
   { code: 'done', description: 'Done', statuses: ['CLOSED', 'DUPLICATE', 'NOT_REPORTABLE'] },
 ] as const
 
-/** Work list options */
-export type WorkListDetails = (typeof workListMapping)[number]
-
 /** Codes for work list */
-export type WorkList = WorkListDetails['code']
-// TODO: WorkList is actually just `string` so offers no type code-based checking
+export type WorkList = (typeof workLists)[number]['code']
 
-export type WorkListStatusMapping = { [key: WorkList]: Status[] }
+type WorkListMapping = Record<WorkList, Status[]>
 
-export const workListStatusMapping: WorkListStatusMapping = workListMapping.reduce(
-  (prev, workListOption) => ({ ...prev, [workListOption.code]: workListOption.statuses }),
-  {},
+/** Work list to status mapping */
+export const workListMapping = workLists.reduce(
+  (mapping, workList) => ({ ...mapping, [workList.code]: workList.statuses }),
+  {} as WorkListMapping,
 )
 
-export const workListCodes = workListMapping.map(mapping => mapping.code)
+/** Work list codes */
+export const workListCodes = workLists.map(workList => workList.code)
 
 // TODO: map these final statuses appropriately if necessary – not needed yet and they will eventually be removed from NOMIS
 // { code: 'POST_INCIDENT_UPDATE', description: 'Post-incident update', nomisCode: 'PIU' },

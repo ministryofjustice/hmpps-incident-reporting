a61d492b9d49b0705fbae5c6546a6999e70ead9d
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2025-07-08 15:00:33 +0100

    Revert "Fix test for logoutUnless"
    This reverts commit e664788b014ee654218413081a8a7aa67844b704.

--- server/middleware/permissions/logoutUnless.test.ts
+++ server/middleware/permissions/logoutUnless.test.ts
@@ -2,17 +2,9 @@ import type { Request, Response, NextFunction } from 'express'
 
 import { logoutUnless } from './logoutUnless'
 import { Permissions } from './rulesClass'
-import { PrisonApi, SERVICE_ALL_PRISONS } from '../../data/prisonApi'
-
-let prisonApi: PrisonApi
 
 describe('Conditional logout', () => {
-  beforeAll(() => {
-    prisonApi = new PrisonApi('token')
-    prisonApi.getServicePrisonIds = jest.fn().mockResolvedValue([SERVICE_ALL_PRISONS])
-  })
-
-  it('should send a forbidden 403 error to next request handler if condition evaluates to false', async () => {
+  it('should send a forbidden 403 error to next request handler if condition evaluates to false', () => {
     const middleware = logoutUnless((permissions, res) => {
       expect(permissions).toBeInstanceOf(Permissions)
       expect(res.locals).toHaveProperty('user')
@@ -20,36 +12,36 @@ describe('Conditional logout', () => {
     })
 
     const req = {} as Request
-    const res = { locals: { user: undefined, apis: { prisonApi } } } as Response
+    const res = { locals: { user: undefined } } as Response
     const next: NextFunction = jest.fn()
 
-    await Permissions.middleware(req, res, next)
+    Permissions.middleware(req, res, next)
     middleware(req, res, next)
 
     expect(next).toHaveBeenCalledWith(expect.objectContaining({ message: 'Forbidden', status: 403 }))
   })
 
-  it('should forward to next request handler if condition evaluates to true', async () => {
+  it('should forward to next request handler if condition evaluates to true', () => {
     const middleware = logoutUnless(permissions => {
       expect(permissions).toBeInstanceOf(Permissions)
       return true
     })
 
     const req = {} as Request
-    const res = { locals: { user: undefined, apis: { prisonApi } } } as Response
+    const res = { locals: { user: undefined } } as Response
     const next: NextFunction = jest.fn()
 
-    await Permissions.middleware(req, res, next)
+    Permissions.middleware(req, res, next)
     middleware(req, res, next)
 
     expect(next).toHaveBeenCalledWith()
   })
 
-  it('should fail if permissions class is not supplied', async () => {
+  it('should fail if permissions class is not supplied', () => {
     const middleware = logoutUnless(() => true)
 
     const req = {} as Request
-    const res = { locals: { user: undefined, apis: { prisonApi } } } as Response
+    const res = { locals: { user: undefined } } as Response
     const next: NextFunction = jest.fn()
 
     // pretend that Permissions.middleware was forgotten

9b3103ae4635e6363d2428afadfab1df8a46942c
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2025-07-10 18:24:52 +0100

    updateActiveAgencies: Improved types and documentation

--- server/middleware/updateActiveAgencies.ts
+++ server/middleware/updateActiveAgencies.ts
@@ -1,4 +1,4 @@
-import type { RequestHandler } from 'express'
+import type { Request, Response, NextFunction, RequestHandler } from 'express'
 
 import { PrisonApi } from '../data/prisonApi'
 import { type Services } from '../services'
@@ -8,12 +8,12 @@ import { setActiveAgencies } from '../data/activeAgencies'
  * Middleware to update the agencies where service is active
  *
  * Gets a fresh copy of the active agencies, using `prisonApi.getAgenciesSwitchedOn()`
- * Sets `res.locals.activeAgencies` but also the `applicationInfo` (which is a
+ * Sets `activeAgencies` but also the `applicationInfo` (which is a
  * singleton with `the additionalFields.activeAgencies` being updated when
- * `activeAgencies` change)
+ * `activeAgencies` changes)
  */
 export default function updateActiveAgencies({ hmppsAuthClient, applicationInfo }: Services): RequestHandler {
-  return async (_req, res, next) => {
+  return async (_req: Request, _res: Response, next: NextFunction) => {
     const systemToken = await hmppsAuthClient.getToken()
     const prisonApi = new PrisonApi(systemToken)
 

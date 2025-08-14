edc070f320a31e24d1ab764598f9abd0479d9f54
Igor <igor.ushkarev1@justice.gov.uk>
2025-08-14 16:34:07 +0100

    Assert that correct list of actions is shown to data wardens for invalid reports

--- integration_tests/e2e/journeys/actionSubmittedReport.cy.ts
+++ integration_tests/e2e/journeys/actionSubmittedReport.cy.ts
@@ -120,106 +120,143 @@ describe('Actioning submitted reports', () => {
   ] as const
   scenarios.forEach(({ currentStatus, sentBackTo }) => {
     context(`a data warden viewing a report with status ${currentStatus}`, () => {
-      const reportWithDetails = { ...validReport, status: currentStatus }
-
       beforeEach(() => {
         cy.resetBasicStubs({ user: mockDataWarden })
         cy.task('stubPrisonApiMockPrison', moorland)
         cy.task('stubManageKnownUsers')
         cy.signIn()
-
-        cy.task('stubIncidentReportingApiGetReportWithDetailsById', { report: reportWithDetails })
-        cy.visit(`/reports/${reportWithDetails.id}`)
       })
 
-      it('should see a list of actions', () => {
-        const reportPage = Page.verifyOnPage(ReportPage, reportWithDetails.reportReference)
-        reportPage.userActionsChoices.then(choices => {
-          const expectedChoices: typeof choices = [
-            { label: 'Close', value: 'CLOSE', checked: false },
-            { label: 'Send back', value: 'REQUEST_CORRECTION', checked: false },
-          ]
-          if (!['ON_HOLD', 'WAS_CLOSED'].includes(currentStatus)) {
-            expectedChoices.push({ label: 'Put on hold', value: 'HOLD', checked: false })
-          }
-          expectedChoices.push(
-            { label: 'Mark as a duplicate', value: 'MARK_DUPLICATE', checked: false },
-            { label: 'Mark as not reportable', value: 'MARK_NOT_REPORTABLE', checked: false },
-          )
-          expect(choices).to.deep.equal(expectedChoices)
+      context('if it is valid (all requirements fulfilled)', () => {
+        const reportWithDetails: DatesAsStrings<ReportWithDetails> = {
+          ...validReport,
+          status: currentStatus,
+        }
+
+        beforeEach(() => {
+          cy.task('stubIncidentReportingApiGetReportWithDetailsById', { report: reportWithDetails })
+          cy.visit(`/reports/${reportWithDetails.id}`)
         })
-      })
 
-      it(`should be able to send it back to ${sentBackTo}`, () => {
-        actionTestCase({
-          reportWithDetails,
-          actionLabel: 'Send back',
-          userAction: 'REQUEST_CORRECTION',
-          newStatus: sentBackTo,
-          comment: 'Add prisoner number to description',
-          banner: 'Incident report 6544 has been sent back',
-        })
-      })
-
-      it(`should be able to close it`, () => {
-        actionTestCase({
-          reportWithDetails,
-          actionLabel: 'Close',
-          userAction: 'CLOSE',
-          newStatus: 'CLOSED',
-          banner: 'Incident report 6544 has been marked as closed',
-        })
-      })
-
-      if (['AWAITING_REVIEW', 'UPDATED'].includes(currentStatus)) {
-        it('should be able to put it on hold', () => {
-          actionTestCase({
-            reportWithDetails,
-            actionLabel: 'Put on hold',
-            userAction: 'HOLD',
-            newStatus: 'ON_HOLD',
-            comment: 'Checking policy…',
-            banner: 'Incident report 6544 has been put on hold',
+        it('should see a list of actions', () => {
+          const reportPage = Page.verifyOnPage(ReportPage, reportWithDetails.reportReference)
+          reportPage.userActionsChoices.then(choices => {
+            const expectedChoices: typeof choices = [
+              { label: 'Close', value: 'CLOSE', checked: false },
+              { label: 'Send back', value: 'REQUEST_CORRECTION', checked: false },
+            ]
+            if (!['ON_HOLD', 'WAS_CLOSED'].includes(currentStatus)) {
+              expectedChoices.push({ label: 'Put on hold', value: 'HOLD', checked: false })
+            }
+            expectedChoices.push(
+              { label: 'Mark as a duplicate', value: 'MARK_DUPLICATE', checked: false },
+              { label: 'Mark as not reportable', value: 'MARK_NOT_REPORTABLE', checked: false },
+            )
+            expect(choices).to.deep.equal(expectedChoices)
           })
         })
-      }
 
-      it('should be able to mark it as a duplicate (without being requested to do so)', () => {
-        const duplicatedReportId = '0198a59c-1111-2222-3333-444444444444'
-        cy.task('stubIncidentReportingApiGetReportByReference', {
-          report: {
-            ...reportWithDetails,
-            id: duplicatedReportId,
-            reportReference: '6543',
-          },
-        })
-        cy.task('stubIncidentReportingApiUpdateReport', {
-          request: { duplicatedReportId },
-          report: {
-            ...reportWithDetails,
-            duplicatedReportId,
-          },
+        it(`should be able to send it back to ${sentBackTo}`, () => {
+          actionTestCase({
+            reportWithDetails,
+            actionLabel: 'Send back',
+            userAction: 'REQUEST_CORRECTION',
+            newStatus: sentBackTo,
+            comment: 'Add prisoner number to description',
+            banner: 'Incident report 6544 has been sent back',
+          })
         })
 
-        actionTestCase({
-          reportWithDetails,
-          actionLabel: 'Mark as a duplicate',
-          userAction: 'MARK_DUPLICATE',
-          newStatus: 'DUPLICATE',
-          originalReportReference: '6543',
-          commentPlaceholder: '(Report is a duplicate of 6543)',
-          banner: 'Report 6544 has been marked as duplicate',
+        it(`should be able to close it`, () => {
+          actionTestCase({
+            reportWithDetails,
+            actionLabel: 'Close',
+            userAction: 'CLOSE',
+            newStatus: 'CLOSED',
+            banner: 'Incident report 6544 has been marked as closed',
+          })
+        })
+
+        if (['AWAITING_REVIEW', 'UPDATED'].includes(currentStatus)) {
+          it('should be able to put it on hold', () => {
+            actionTestCase({
+              reportWithDetails,
+              actionLabel: 'Put on hold',
+              userAction: 'HOLD',
+              newStatus: 'ON_HOLD',
+              comment: 'Checking policy…',
+              banner: 'Incident report 6544 has been put on hold',
+            })
+          })
+        }
+
+        it('should be able to mark it as a duplicate (without being requested to do so)', () => {
+          const duplicatedReportId = '0198a59c-1111-2222-3333-444444444444'
+          cy.task('stubIncidentReportingApiGetReportByReference', {
+            report: {
+              ...reportWithDetails,
+              id: duplicatedReportId,
+              reportReference: '6543',
+            },
+          })
+          cy.task('stubIncidentReportingApiUpdateReport', {
+            request: { duplicatedReportId },
+            report: {
+              ...reportWithDetails,
+              duplicatedReportId,
+            },
+          })
+
+          actionTestCase({
+            reportWithDetails,
+            actionLabel: 'Mark as a duplicate',
+            userAction: 'MARK_DUPLICATE',
+            newStatus: 'DUPLICATE',
+            originalReportReference: '6543',
+            commentPlaceholder: '(Report is a duplicate of 6543)',
+            banner: 'Report 6544 has been marked as duplicate',
+          })
+        })
+
+        it('should be able to mark it as not reportable (without being requested to do so)', () => {
+          actionTestCase({
+            reportWithDetails,
+            actionLabel: 'Mark as not reportable',
+            userAction: 'MARK_NOT_REPORTABLE',
+            newStatus: 'NOT_REPORTABLE',
+            commentPlaceholder: '(Not reportable)',
+            banner: 'Report 6544 has been marked as not reportable',
+          })
         })
       })
 
-      it('should be able to mark it as not reportable (without being requested to do so)', () => {
-        actionTestCase({
-          reportWithDetails,
-          actionLabel: 'Mark as not reportable',
-          userAction: 'MARK_NOT_REPORTABLE',
-          newStatus: 'NOT_REPORTABLE',
-          commentPlaceholder: '(Not reportable)',
-          banner: 'Report 6544 has been marked as not reportable',
+      context('if it is invalid (questions not answered)', () => {
+        const reportWithDetails: DatesAsStrings<ReportWithDetails> = {
+          ...validReport,
+          status: currentStatus,
+          questions: [],
+        }
+
+        beforeEach(() => {
+          cy.task('stubIncidentReportingApiGetReportWithDetailsById', { report: reportWithDetails })
+          cy.visit(`/reports/${reportWithDetails.id}`)
+        })
+
+        it('should see a list of actions', () => {
+          const reportPage = Page.verifyOnPage(ReportPage, reportWithDetails.reportReference)
+          reportPage.userActionsChoices.then(choices => {
+            const expectedChoices: typeof choices = [
+              { label: 'Send back', value: 'REQUEST_CORRECTION', checked: false },
+            ]
+            if (!['ON_HOLD', 'WAS_CLOSED'].includes(currentStatus)) {
+              expectedChoices.push({ label: 'Put on hold', value: 'HOLD', checked: false })
+            }
+            expectedChoices.push(
+              { label: 'Mark as a duplicate', value: 'MARK_DUPLICATE', checked: false },
+              { label: 'Mark as not reportable', value: 'MARK_NOT_REPORTABLE', checked: false },
+            )
+            expect(choices).to.deep.equal(expectedChoices)
+          })
         })
       })
     })

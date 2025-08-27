4560c21721c533e34ae6ec8948de10d13f4c9c92
Igor <igor.ushkarev1@justice.gov.uk>
2025-08-27 12:56:22 +0100

    Test home page for different user types

--- integration_tests/e2e/home.cy.ts
+++ integration_tests/e2e/home.cy.ts
@@ -1,21 +1,39 @@
+import { mockDataWarden, mockReportingOfficer, mockHqViewer } from '../../server/data/testData/users'
 import Page from '../pages/page'
 import { HomePage } from '../pages/home'
 
-context('Home page', () => {
-  beforeEach(() => {
-    cy.resetBasicStubs()
-    cy.signIn()
-  })
+const scenarios: Scenario[] = [
+  { userType: 'reporting officers', user: mockReportingOfficer },
+  { userType: 'data wardens', user: mockDataWarden },
+  { userType: 'HQ viewers', user: mockHqViewer },
+]
+for (const { userType, user } of scenarios) {
+  context(`Home page for ${userType}`, () => {
+    beforeEach(() => {
+      cy.resetBasicStubs({ user })
+      cy.signIn()
+    })
 
-  it('should show tiles & breadcrumbs', () => {
-    const homePage = Page.verifyOnPage(HomePage)
-    homePage.checkLastBreadcrumb('Digital Prison Services')
-    homePage.cardDetails.then(cards => {
-      expect(cards).to.deep.equal([
-        { title: 'Create an incident report', url: '/create-report' },
-        { title: 'Search incident reports', url: '/reports' },
-        { title: 'Management reporting', url: '/management-reporting' },
-      ])
+    it('should show tiles & breadcrumbs', () => {
+      const homePage = Page.verifyOnPage(HomePage)
+      homePage.checkLastBreadcrumb('Digital Prison Services')
+      homePage.cardDetails.then(cards => {
+        const expectedTiles = [
+          { title: 'Search incident reports', url: '/reports' },
+          { title: 'Management reporting', url: '/management-reporting' },
+        ]
+        if (userType === 'reporting officers') {
+          expectedTiles.unshift({ title: 'Create an incident report', url: '/create-report' })
+        } else if (userType === 'data wardens') {
+          expectedTiles.unshift({ title: 'Create a PECS incident report', url: '/create-report/pecs' })
+        }
+        expect(cards).to.deep.equal(expectedTiles)
+      })
     })
   })
-})
+}
+
+interface Scenario {
+  userType: string
+  user: Express.User
+}

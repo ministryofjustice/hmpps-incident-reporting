82cb8fe602c39eba0be9f74bc3f0c71bbcb7205a
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-10-15 14:00:46 +0100

    IR-1257 ✅ Add tests for handling NOU region in PECS permissions logic.
    - Updated `mockPecsRegions` to conditionally include the NOU region.
    - Enhanced test coverage for permissions in different regions and scenarios.

--- server/data/testData/pecsRegions.ts
+++ server/data/testData/pecsRegions.ts
@@ -7,9 +7,13 @@ export const pecsSouthRegion: PecsRegion = fromAgency(pecsSouth)
 
 const backupPecsRegions: PecsRegion[] = []
 
-export function mockPecsRegions() {
+export function mockPecsRegions(addNou: boolean = false) {
   backupPecsRegions.push(...pecsRegions)
-  pecsRegions.splice(0, pecsRegions.length, nouRegion, pecsNorthRegion, pecsSouthRegion)
+  if (addNou) {
+    pecsRegions.splice(0, pecsRegions.length, nouRegion, pecsNorthRegion, pecsSouthRegion)
+  } else {
+    pecsRegions.splice(0, pecsRegions.length, pecsNorthRegion, pecsSouthRegion)
+  }
 }
 
 export function resetPecsRegions() {

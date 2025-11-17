94f73a4b7cd0eafe59a0e3dcbd21df1d8464e746
Michael Willis <Michael.Willis2@justice.gov.uk>
2025-11-17 17:36:08 +0000

    IR-1308 Update prisoner search tests to include additional caseloads (OUT, TRN) in global search stubs

--- integration_tests/e2e/involvements/prisoners/search.cy.ts
+++ integration_tests/e2e/involvements/prisoners/search.cy.ts
@@ -105,7 +105,7 @@ context('Prisoner search page', () => {
   context('when searching globally', () => {
     context('and nobody was found', () => {
       beforeEach(() => {
-        cy.task('stubOffenderSearchGlobally', { andWords: 'A1111', results: [] })
+        cy.task('stubOffenderSearchGlobally', { andWords: 'A1111', prisonIds: ['MDI', 'OUT', 'TRN'], results: [] })
 
         cy.visit(`/reports/${reportWithDetails.id}/prisoners/search?page=1&global=yes&q=A1111`)
         prisonerSearchPage = Page.verifyOnPage(PrisonerSearchPage)
@@ -122,7 +122,11 @@ context('Prisoner search page', () => {
 
     context('and results were returned', () => {
       beforeEach(() => {
-        cy.task('stubOffenderSearchGlobally', { andWords: 'A1111', results: [andrew] })
+        cy.task('stubOffenderSearchGlobally', {
+          andWords: 'A1111',
+          prisonIds: ['MDI', 'OUT', 'TRN'],
+          results: [andrew],
+        })
         cy.task('stubPrisonApiMockPrisonerPhoto', andrew.prisonerNumber)
 
         cy.visit(`/reports/${reportWithDetails.id}/prisoners/search?page=1&global=yes&q=A1111`)

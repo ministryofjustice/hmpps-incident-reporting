03ca5ef2fb2a231005605f88c2dd22dd15c8e8fe
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2026-03-02 14:50:23 +0000

    DRP: Create services

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,16 +1,46 @@
+import { createDprServices } from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/utils/CreateDprServices'
+
 import { dataAccess } from '../data'
 import UserService from './userService'
 
 export const services = () => {
-  const { applicationInfo, hmppsAuthClient, manageUsersApiClient, frontendComponentsClient } = dataAccess()
+  const {
+    applicationInfo,
+    hmppsAuthClient,
+    manageUsersApiClient,
+    frontendComponentsClient,
+    // DPR
+    reportingClient,
+    dashboardClient,
+    reportDataStore,
+    featureFlagService,
+    productCollectionClient,
+    missingReportClient,
+  } = dataAccess()
 
   const userService = new UserService(manageUsersApiClient)
+
+  const dprClients = {
+    reportingClient,
+    dashboardClient,
+    reportDataStore,
+    featureFlagService,
+    productCollectionClient,
+    missingReportClient,
+  }
+  const featureConfig = {
+    bookmarking: true, // Enables bookmarking feature
+    download: true, // Enables download feature
+    saveDefaults: true, // Enables save user defaults feature
+  }
+  const dprServices = createDprServices(dprClients, featureConfig)
 
   return {
     applicationInfo,
     hmppsAuthClient,
     userService,
     frontendComponentsClient,
+    ...dprServices,
   }
 }
 

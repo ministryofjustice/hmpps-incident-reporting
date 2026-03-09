1910f0b49104cd0d558ab4a8b5940b52ce0faaa7
James Reed <james.reed1@justice.gov.uk>
2026-03-09 12:18:46 +0000

    fix tsconfigs, update dpr to v5

--- server/routes/dpr/index.ts
+++ server/routes/dpr/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from 'express'
-import ReportListUtils from '@ministryofjustice/hmpps-digital-prison-reporting-frontend/dpr/components/report-list/utils'
 import config from '../../config'
 import { IncidentReportingApi, ManagementReportDefinition } from '../../data/incidentReportingApi'
 import { Services } from '../../services'
@@ -11,47 +10,47 @@ async function getIncidentReportingApi(services: Services): Promise<IncidentRepo
   return new IncidentReportingApi(systemToken)
 }
 
-async function populateRoutes(
-  incidentReportingApi: IncidentReportingApi,
-  router: Router,
-): Promise<ManagementReportDefinition[]> {
-  const allDefinitions = await incidentReportingApi.getManagementReportDefinitions()
-  if (definitionsRoutesInitialised === false) {
-    for (const definition of allDefinitions) {
-      for (const variant of definition.variants) {
-        router.get(
-          `/management-reporting/${definition.id}-${variant.id}`,
-          ReportListUtils.createReportListRequestHandler({
-            title: variant.name,
-            definitionName: definition.id,
-            variantName: variant.id,
-            apiUrl: config.apis.hmppsIncidentReportingApi.url,
-            apiTimeout: config.apis.hmppsIncidentReportingApi.timeout.deadline,
-            layoutTemplate: 'partials/dprLayout.njk',
-            tokenProvider: (request: Request, response: Response) => {
-              return response.locals.systemToken
-            },
-          }),
-        )
-      }
-    }
-    definitionsRoutesInitialised = true
-  }
-  return allDefinitions
-}
+// async function populateRoutes(
+//   incidentReportingApi: IncidentReportingApi,
+//   router: Router,
+// ): Promise<ManagementReportDefinition[]> {
+//   const allDefinitions = await incidentReportingApi.getManagementReportDefinitions()
+//   if (definitionsRoutesInitialised === false) {
+//     for (const definition of allDefinitions) {
+//       for (const variant of definition.variants) {
+//         router.get(
+//           `/management-reporting/${definition.id}-${variant.id}`,
+//           ReportListUtils.createReportListRequestHandler({
+//             title: variant.name,
+//             definitionName: definition.id,
+//             variantName: variant.id,
+//             apiUrl: config.apis.hmppsIncidentReportingApi.url,
+//             apiTimeout: config.apis.hmppsIncidentReportingApi.timeout.deadline,
+//             layoutTemplate: 'partials/dprLayout.njk',
+//             tokenProvider: (request: Request, response: Response) => {
+//               return response.locals.systemToken
+//             },
+//           }),
+//         )
+//       }
+//     }
+//     definitionsRoutesInitialised = true
+//   }
+//   return allDefinitions
+// }
 
 export function dprRouter(router: Router, services: Services): Router {
   if (config.loadReportDefinitionsOnStartup === true && definitionsRoutesInitialised === false) {
-    getIncidentReportingApi(services).then(incidentReportingApi => populateRoutes(incidentReportingApi, router))
+    // getIncidentReportingApi(services).then(incidentReportingApi => populateRoutes(incidentReportingApi, router))
   }
 
   // TODO: will management reporting need a separate role?
 
-  router.get('/management-reporting', async (req, res) => {
-    const { incidentReportingApi } = res.locals.apis
-    const definitions = await populateRoutes(incidentReportingApi, router)
-    res.render('pages/managementReporting/index', { definitions })
-  })
+  // router.get('/management-reporting', async (req, res) => {
+  //   const { incidentReportingApi } = res.locals.apis
+  //   const definitions = await populateRoutes(incidentReportingApi, router)
+  //   res.render('pages/managementReporting/index', { definitions })
+  // })
 
   return router
 }

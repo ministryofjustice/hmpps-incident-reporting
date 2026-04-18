f80807cdce21ffac727670c10ae3da43c16c83cc
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2026-04-18 13:57:13 +0100

    Remove type definitions already in MoJ's frontend-types
    The `@ministryofjustice/frontend-types` package already
    has most of these type definitions so we should be able
    to remove our own version.
    
    See: https://github.com/ministryofjustice/hmpps-typescript-lib/tree/main/packages/frontend-types

--- server/@types/moj-frontend/index.d.ts
+++ server/@types/moj-frontend/index.d.ts
@@ -4,17 +4,6 @@
  * For example, no concrete component classes are included.
  */
 
-// eslint-disable-next-line max-classes-per-file
-declare module '@ministryofjustice/frontend' {
-  import type { Component, createAll } from 'govuk-frontend'
-
-  export function initAll(scopeOrConfig?: Parameters<createAll>[0]): void
-
-  export class DatePicker extends Component {}
-
-  export class SortableTable extends Component {}
-}
-
 declare module '@ministryofjustice/frontend/moj/filters/all' {
   interface Filters {
     mojDate(

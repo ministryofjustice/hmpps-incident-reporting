527f39873bc5ff8aca6a96cdac4e6fa932327061
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2026-04-30 17:22:31 +0100

    prisonerLocationUtils: Export only the strict necessary
    Modules like libraries should have the smallest possible
    public interface and don't export functionality unless
    it's necessary.
    
    The is `isBeingTransferred()`/`isOutside()` are now only used
    internally by the `prisonerLocation()` utility function.
    
    The `isInPrison()` function wasn't even used anywhere and it
    was probably added "just for consistency", so it is gone
    now (YAGNI, it is trivial to add back **if** necessary).
    
    Tests were also simplified as a result.

--- server/utils/prisonerLocationUtils.test.ts
+++ server/utils/prisonerLocationUtils.test.ts
@@ -1,15 +1,11 @@
 import { andrew, barry, chris, donald, ernie, fred } from '../data/testData/offenderSearch'
-import { isBeingTransferred, isInPrison, isOutside, prisonerLocation } from './prisonerLocationUtils'
+import { prisonerLocation } from './prisonerLocationUtils'
 
 describe('prisoners’ locations', () => {
   it.each([andrew, barry, chris])(
     'for people who are in prison with a known cell location (e.g. $cellLocation)',
     prisoner => {
       expect(prisonerLocation(prisoner)).toEqual(prisoner.prisonName)
-
-      expect(isBeingTransferred(prisoner)).toBe(false)
-      expect(isOutside(prisoner)).toBe(false)
-      expect(isInPrison(prisoner)).toBe(true)
     },
   )
 
@@ -17,46 +13,26 @@ describe('prisoners’ locations', () => {
     const prisoner = { ...andrew }
     delete prisoner.cellLocation
     expect(prisonerLocation(prisoner)).toEqual(prisoner.prisonName)
-
-    expect(isBeingTransferred(prisoner)).toBe(false)
-    expect(isOutside(prisoner)).toBe(false)
-    expect(isInPrison(prisoner)).toBe(true)
   })
 
   it('for people being transferred', () => {
     expect(prisonerLocation(donald)).toEqual('N/A')
-
-    expect(isBeingTransferred(donald)).toBe(true)
-    expect(isOutside(donald)).toBe(false)
-    expect(isInPrison(donald)).toBe(false)
   })
 
   it('for people being transferred without a location description', () => {
     const prisoner = { ...donald }
     delete prisoner.locationDescription
     expect(prisonerLocation(prisoner)).toEqual('N/A')
-
-    expect(isBeingTransferred(prisoner)).toBe(true)
-    expect(isOutside(prisoner)).toBe(false)
-    expect(isInPrison(prisoner)).toBe(false)
   })
 
   it('for people outside prison', () => {
     expect(prisonerLocation(ernie)).toEqual('Outside - released from Moorland (HMP)')
-
-    expect(isBeingTransferred(ernie)).toBe(false)
-    expect(isOutside(ernie)).toBe(true)
-    expect(isInPrison(ernie)).toBe(false)
   })
 
   it('for people outside prison without a location description', () => {
     const prisoner = { ...ernie }
     delete prisoner.locationDescription
     expect(prisonerLocation(prisoner)).toEqual('Outside')
-
-    expect(isBeingTransferred(prisoner)).toBe(false)
-    expect(isOutside(prisoner)).toBe(true)
-    expect(isInPrison(prisoner)).toBe(false)
   })
 
   it('for people whose location is blank', () => {
@@ -67,17 +43,9 @@ describe('prisoners’ locations', () => {
       cellLocation: '',
     }
     expect(prisonerLocation(prisoner)).toEqual('Not known')
-
-    expect(isBeingTransferred(prisoner)).toBe(false)
-    expect(isOutside(prisoner)).toBe(false)
-    expect(isInPrison(prisoner)).toBe(false)
   })
 
   it('for people whose location is undefined', () => {
     expect(prisonerLocation(fred)).toEqual('Not known')
-
-    expect(isBeingTransferred(fred)).toBe(false)
-    expect(isOutside(fred)).toBe(false)
-    expect(isInPrison(fred)).toBe(false)
   })
 })

eba149f5663724eaa440deb92c65f8943accdef1
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2026-06-05 12:25:17 +0100

    Various type tweaks related to `res.locals.user`
    There were a couple of errors related to `res.locals.user`
    possibly being `undefined`. By the look of how middleware
    are wired the reality of things is that user should be
    defined (or you'd be unauthenticated and redirected to
    the login pageunauthenticated).
    
    The `as HmppsUser` in the `setUpAuthentication()`
    middleware was copied over from the TS template, at that point
    the user would be defined and come from HMPPS Auth:
    
    https://github.com/ministryofjustice/hmpps-template-typescript/blob/ac5a2ed78487a81c7b80b39f269feaf3609e4615/server/middleware/setUpAuthentication.ts#L86
    
    There was also a place in `UserService` where the `null`
    users are filtered out but TS can't follow along, in that
    case I've used a null assertion (!), even tho this is
    not ideal and TS should look through the `filter()`.

--- server/middleware/permissions/logoutUnless.test.ts
+++ server/middleware/permissions/logoutUnless.test.ts
@@ -12,7 +12,7 @@ describe('Conditional logout', () => {
     })
 
     const req = {} as Request
-    const res = { locals: { user: undefined } } as Response
+    const res = { locals: { user: {} } } as Response
     const next: NextFunction = jest.fn()
 
     Permissions.middleware(req, res, next)
@@ -28,7 +28,7 @@ describe('Conditional logout', () => {
     })
 
     const req = {} as Request
-    const res = { locals: { user: undefined } } as Response
+    const res = { locals: { user: {} } } as Response
     const next: NextFunction = jest.fn()
 
     Permissions.middleware(req, res, next)
@@ -41,7 +41,7 @@ describe('Conditional logout', () => {
     const middleware = logoutUnless(() => true)
 
     const req = {} as Request
-    const res = { locals: { user: undefined } } as Response
+    const res = { locals: { user: {} } } as Response
     const next: NextFunction = jest.fn()
 
     // pretend that Permissions.middleware was forgotten

42a1c011f9c527cef3d7f60f775aebb4ef6f9f4a
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2026-07-03 15:35:28 +0100

    PoC: Move Dashboard validations out of req handler
    Trying to simplify this big/long request handler.

--- server/routes/dashboard/filters.ts
+++ server/routes/dashboard/filters.ts
@@ -1,10 +1,15 @@
 import { type Session, type SessionData } from 'express-session'
 import { type ParsedQs } from 'qs'
 
+import { type GovukErrorSummaryItem } from '../../utils/govukFrontend'
+import { typeFamilies, TypeFamily, types } from '../../reportConfiguration/constants'
+
 interface UiFilters {
   searchID?: string
+  location?: string
   fromDate?: string
   toDate?: string
+  typeFamily?: TypeFamily
 }
 
 export function readUiFilters({
@@ -18,17 +23,41 @@ export function readUiFilters({
 }): UiFilters {
   const uiFilters = {
     searchID: typeof query.searchID === 'string' ? query.searchID.trim() : undefined,
+    location: typeof query.location === 'string' ? query.location : undefined,
     fromDate: typeof query.fromDate === 'string' ? query.fromDate : undefined,
     toDate: typeof query.toDate === 'string' ? query.toDate : undefined,
+    typeFamily: query.typeFamily as TypeFamily | undefined,
   }
 
   // If no filters are supplied from query, check for filters in session
   if (url === '/' && session.dashboardFilters) {
     const sessionFilters = session.dashboardFilters
     uiFilters.searchID = sessionFilters?.searchID
+    uiFilters.location = sessionFilters?.location
     uiFilters.fromDate = sessionFilters?.fromDate
     uiFilters.toDate = sessionFilters?.toDate
+    uiFilters.typeFamily = sessionFilters?.typeFamily
   }
 
   return uiFilters
 }
+
+export function validateUiFilters(uiFilters: UiFilters): GovukErrorSummaryItem[] {
+  const errors: GovukErrorSummaryItem[] = []
+
+  if (uiFilters.typeFamily && !(uiFilters.typeFamily in familyToType)) {
+    errors.push({ href: '#typeFamily', text: 'Select a valid incident type' })
+  }
+
+  return errors
+}
+
+/** Given a family code, list type codes belonging to the family */
+export const familyToType = Object.fromEntries(
+  Object.values(typeFamilies).map(({ code: familyCode }) => [
+    familyCode,
+    Object.values(types)
+      .filter(({ familyCode: someFamilyCode }) => someFamilyCode === familyCode)
+      .map(({ code }) => code),
+  ]),
+)

2acf4f0ac513ad582dffc2163d96aefb31a862be
Josh Jones <josh.jones4@justice.gov.uk>
2026-09-09 16:09:30 +0100

    Reshaping types object and corresponding type derivations

--- server/reportConfiguration/constants/types.ts
+++ server/reportConfiguration/constants/types.ts
@@ -1,319 +1,307 @@
+import { TypeFamily } from './typeFamilies'
+
+export type TypeDetails = {
+  familyCode: TypeFamily
+  description: string
+  active: boolean
+  nomisCode: string
+}
+
 /** Types of reportable incidents */
-export const types = [
-  { familyCode: 'ABSCOND', code: 'ABSCOND_1', description: 'Abscond', active: true, nomisCode: 'ABSCOND' },
-  { familyCode: 'ASSAULT', code: 'ASSAULT_1', description: 'Assault', active: false, nomisCode: 'ASSAULT' },
-  { familyCode: 'ASSAULT', code: 'ASSAULT_2', description: 'Assault', active: false, nomisCode: 'ASSAULTS' },
-  { familyCode: 'ASSAULT', code: 'ASSAULT_3', description: 'Assault', active: false, nomisCode: 'ASSAULTS1' },
-  { familyCode: 'ASSAULT', code: 'ASSAULT_4', description: 'Assault', active: false, nomisCode: 'ASSAULTS2' },
-  { familyCode: 'ASSAULT', code: 'ASSAULT_5', description: 'Assault', active: true, nomisCode: 'ASSAULTS3' },
-  {
+export const types = {
+  ABSCOND_1: { familyCode: 'ABSCOND', description: 'Abscond', active: true, nomisCode: 'ABSCOND' },
+  ASSAULT_1: { familyCode: 'ASSAULT', description: 'Assault', active: false, nomisCode: 'ASSAULT' },
+  ASSAULT_2: { familyCode: 'ASSAULT', description: 'Assault', active: false, nomisCode: 'ASSAULTS' },
+  ASSAULT_3: { familyCode: 'ASSAULT', description: 'Assault', active: false, nomisCode: 'ASSAULTS1' },
+  ASSAULT_4: { familyCode: 'ASSAULT', description: 'Assault', active: false, nomisCode: 'ASSAULTS2' },
+  ASSAULT_5: {
+    familyCode: 'ASSAULT',
+    description: 'Assault',
+    active: true,
+    nomisCode: 'ASSAULTS3',
+  },
+  ATTEMPTED_ESCAPE_FROM_PRISON_1: {
     familyCode: 'ATTEMPTED_ESCAPE_FROM_PRISON',
-    code: 'ATTEMPTED_ESCAPE_FROM_PRISON_1',
     description: 'Attempted escape from establishment',
     active: true,
     nomisCode: 'ATT_ESCAPE',
   },
-  {
+  ATTEMPTED_ESCAPE_FROM_ESCORT_1: {
     familyCode: 'ATTEMPTED_ESCAPE_FROM_ESCORT',
-    code: 'ATTEMPTED_ESCAPE_FROM_ESCORT_1',
     description: 'Attempted escape from escort',
     active: true,
     nomisCode: 'ATT_ESC_E',
   },
-  { familyCode: 'BARRICADE', code: 'BARRICADE_1', description: 'Barricade', active: false, nomisCode: 'BARRICADE' },
-  { familyCode: 'BOMB', code: 'BOMB_1', description: 'Bomb explosion or threat', active: true, nomisCode: 'BOMB' },
-  {
+  BARRICADE_1: { familyCode: 'BARRICADE', description: 'Barricade', active: false, nomisCode: 'BARRICADE' },
+  BOMB_1: { familyCode: 'BOMB', description: 'Bomb explosion or threat', active: true, nomisCode: 'BOMB' },
+  BREACH_OF_SECURITY_1: {
     familyCode: 'BREACH_OF_SECURITY',
-    code: 'BREACH_OF_SECURITY_1',
     description: 'Breach or attempted breach of security',
     active: true,
     nomisCode: 'BREACH',
   },
-  {
+  BC_DISRUPT_3RD_PTY_1: {
     familyCode: 'BC_DISRUPT_3RD_PTY',
-    code: 'BC_DISRUPT_3RD_PTY_1',
     description: 'Business Continuity - Disruption to 3rd party supplier',
     active: true,
     nomisCode: 'DIS_3RD_PTY',
   },
-  {
+  BC_FUEL_SHORTAGE_1: {
     familyCode: 'BC_FUEL_SHORTAGE',
-    code: 'BC_FUEL_SHORTAGE_1',
     description: 'Business Continuity - Fuel shortage',
     active: true,
     nomisCode: 'FUELSHORTAGE',
   },
-  {
+  BC_LOSS_ACCESS_EGRESS_1: {
     familyCode: 'BC_LOSS_ACCESS_EGRESS',
-    code: 'BC_LOSS_ACCESS_EGRESS_1',
     description: 'Business Continuity - Loss of access / egress',
     active: true,
     nomisCode: 'LOSS_EGRESS',
   },
-  {
+  BC_LOSS_COMMS_1: {
     familyCode: 'BC_LOSS_COMMS',
-    code: 'BC_LOSS_COMMS_1',
     description: 'Business Continuity - Loss of communications & digital systems',
     active: true,
     nomisCode: 'LOSS_COMMS',
   },
-  {
+  BC_LOSS_UTILS_1: {
     familyCode: 'BC_LOSS_UTILS',
-    code: 'BC_LOSS_UTILS_1',
     description: 'Business Continuity - Loss of utilities',
     active: true,
     nomisCode: 'LOSS_UTILS',
   },
-  {
+  BC_SERV_WEATHER_1: {
     familyCode: 'BC_SERV_WEATHER',
-    code: 'BC_SERV_WEATHER_1',
     description: 'Business Continuity - Severe weather',
     active: true,
     nomisCode: 'SERV_WEATHER',
   },
-  {
+  BC_STAFF_SHORTAGES_1: {
     familyCode: 'BC_STAFF_SHORTAGES',
-    code: 'BC_STAFF_SHORTAGES_1',
     description: 'Business Continuity - Staff shortages',
     active: true,
     nomisCode: 'STF_SHORTAGE',
   },
-  {
+  BC_WIDESPREAD_ILLNESS_1: {
     familyCode: 'BC_WIDESPREAD_ILLNESS',
-    code: 'BC_WIDESPREAD_ILLNESS_1',
     description: 'Business Continuity - Widespread illness',
     active: true,
     nomisCode: 'WSPR_ILLNESS',
   },
-  {
+  CLOSE_DOWN_SEARCH_1: {
     familyCode: 'CLOSE_DOWN_SEARCH',
-    code: 'CLOSE_DOWN_SEARCH_1',
     description: 'Close down search',
     active: true,
     nomisCode: 'CLOSE_DOWN',
   },
-  {
+  CONCERTED_INDISCIPLINE_1: {
     familyCode: 'CONCERTED_INDISCIPLINE',
-    code: 'CONCERTED_INDISCIPLINE_1',
     description: 'Incident involving 2 or more prisioners acting together',
     active: false,
     nomisCode: 'CON_INDISC',
   },
-  { familyCode: 'DAMAGE', code: 'DAMAGE_1', description: 'Deliberate damage', active: false, nomisCode: 'DAMAGE' },
-  {
+  DAMAGE_1: { familyCode: 'DAMAGE', description: 'Deliberate damage', active: false, nomisCode: 'DAMAGE' },
+  DEATH_PRISONER_1: {
     familyCode: 'DEATH_PRISONER',
-    code: 'DEATH_PRISONER_1',
     description: 'Death of prisoner',
     active: true,
     nomisCode: 'DEATH',
   },
-  {
+  DEATH_OTHER_1: {
     familyCode: 'DEATH_OTHER',
-    code: 'DEATH_OTHER_1',
     description: 'Death of other person',
     active: true,
     nomisCode: 'DEATH_NI',
   },
-  {
+  DIRTY_PROTEST_1: {
     familyCode: 'DIRTY_PROTEST',
-    code: 'DIRTY_PROTEST_1',
     description: 'Dirty protest',
     active: true,
     nomisCode: 'DIRTYPROTEST',
   },
-  { familyCode: 'DISORDER', code: 'DISORDER_1', description: 'Disorder', active: false, nomisCode: 'DISORDER' },
-  { familyCode: 'DISORDER', code: 'DISORDER_2', description: 'Disorder', active: true, nomisCode: 'DISORDER1' },
-  {
+  DISORDER_1: { familyCode: 'DISORDER', description: 'Disorder', active: false, nomisCode: 'DISORDER' },
+  DISORDER_2: {
+    familyCode: 'DISORDER',
+    description: 'Disorder',
+    active: true,
+    nomisCode: 'DISORDER1',
+  },
+  DRONE_SIGHTING_1: {
     familyCode: 'DRONE_SIGHTING',
-    code: 'DRONE_SIGHTING_1',
     description: 'Drone sighting',
     active: false,
     nomisCode: 'DRONE',
   },
-  {
+  DRONE_SIGHTING_2: {
     familyCode: 'DRONE_SIGHTING',
-    code: 'DRONE_SIGHTING_2',
     description: 'Drone sighting',
     active: false,
     nomisCode: 'DRONE1',
   },
-  {
+  DRONE_SIGHTING_3: {
     familyCode: 'DRONE_SIGHTING',
-    code: 'DRONE_SIGHTING_3',
     description: 'Drone sighting',
     active: true,
     nomisCode: 'DRONE2',
   },
-  { familyCode: 'DRUGS', code: 'DRUGS_1', description: 'Drugs', active: false, nomisCode: 'DRUGS' },
-  {
+  DRUGS_1: { familyCode: 'DRUGS', description: 'Drugs', active: false, nomisCode: 'DRUGS' },
+  ESCAPE_FROM_PRISON_1: {
     familyCode: 'ESCAPE_FROM_PRISON',
-    code: 'ESCAPE_FROM_PRISON_1',
     description: 'Escape from establishment',
     active: true,
     nomisCode: 'ESCAPE_EST',
   },
-  {
+  ESCAPE_FROM_ESCORT_1: {
     familyCode: 'ESCAPE_FROM_ESCORT',
-    code: 'ESCAPE_FROM_ESCORT_1',
     description: 'Escape from escort',
     active: true,
     nomisCode: 'ESCAPE_ESC',
   },
-  { familyCode: 'FIND', code: 'FIND_1', description: 'Find of illicit items', active: false, nomisCode: 'FINDS' },
-  { familyCode: 'FIND', code: 'FIND_2', description: 'Find of illicit items', active: false, nomisCode: 'FIND' },
-  { familyCode: 'FIND', code: 'FIND_3', description: 'Find of illicit items', active: false, nomisCode: 'FIND1' },
-  { familyCode: 'FIND', code: 'FIND_4', description: 'Find of illicit items', active: false, nomisCode: 'FIND0322' },
-  { familyCode: 'FIND', code: 'FIND_5', description: 'Find of illicit items', active: false, nomisCode: 'FINDS1' },
-  { familyCode: 'FIND', code: 'FIND_6', description: 'Find of illicit items', active: true, nomisCode: 'FIND0422' },
-  { familyCode: 'FIRE', code: 'FIRE_1', description: 'Fire', active: true, nomisCode: 'FIRE' },
-  {
+  FIND_1: { familyCode: 'FIND', description: 'Find of illicit items', active: false, nomisCode: 'FINDS' },
+  FIND_2: { familyCode: 'FIND', description: 'Find of illicit items', active: false, nomisCode: 'FIND' },
+  FIND_3: { familyCode: 'FIND', description: 'Find of illicit items', active: false, nomisCode: 'FIND1' },
+  FIND_4: { familyCode: 'FIND', description: 'Find of illicit items', active: false, nomisCode: 'FIND0322' },
+  FIND_5: { familyCode: 'FIND', description: 'Find of illicit items', active: false, nomisCode: 'FINDS1' },
+  FIND_6: {
+    familyCode: 'FIND',
+    description: 'Find of illicit items',
+    active: true,
+    nomisCode: 'FIND0422',
+  },
+  FIRE_1: { familyCode: 'FIRE', description: 'Fire', active: true, nomisCode: 'FIRE' },
+  FIREARM_1: {
     familyCode: 'FIREARM',
-    code: 'FIREARM_1',
     description: 'Firearm, ammunition or chemical incapacitant',
     active: false,
     nomisCode: 'FIREARM_ETC',
   },
-  {
+  FOOD_REFUSAL_1: {
     familyCode: 'FOOD_REFUSAL',
-    code: 'FOOD_REFUSAL_1',
     description: 'Food or liquid refusal',
     active: true,
     nomisCode: 'FOOD_REF',
   },
-  {
+  FOOD_REFUSAL_2: {
     familyCode: 'FOOD_REFUSAL',
-    code: 'FOOD_REFUSAL_2',
     description: 'Food or liquid refusal',
     active: true,
     nomisCode: 'FOOD_REF2',
   },
-  { familyCode: 'HOSTAGE', code: 'HOSTAGE_1', description: 'Hostage incident', active: false, nomisCode: 'HOSTAGE' },
-  {
+  HOSTAGE_1: { familyCode: 'HOSTAGE', description: 'Hostage incident', active: false, nomisCode: 'HOSTAGE' },
+  INCIDENT_AT_HEIGHT_1: {
     familyCode: 'INCIDENT_AT_HEIGHT',
-    code: 'INCIDENT_AT_HEIGHT_1',
     description: 'Incident at height',
     active: false,
     nomisCode: 'ROOF_CLIMB',
   },
-  {
+  KEY_OR_LOCK_1: {
     familyCode: 'KEY_OR_LOCK',
-    code: 'KEY_OR_LOCK_1',
     description: 'Key or lock compromise',
     active: false,
     nomisCode: 'KEY_LOCK',
   },
-  {
+  KEY_OR_LOCK_2: {
     familyCode: 'KEY_OR_LOCK',
-    code: 'KEY_OR_LOCK_2',
     description: 'Key or lock compromise',
     active: true,
     nomisCode: 'KEY_LOCKNEW',
   },
-  {
+  KEY_OR_LOCK_3: {
     familyCode: 'KEY_OR_LOCK',
-    code: 'KEY_OR_LOCK_3',
     description: 'Key or lock compromise',
     active: true,
     nomisCode: 'KEY_LOCK3',
   },
-  {
+  MISCELLANEOUS_1: {
     familyCode: 'MISCELLANEOUS',
-    code: 'MISCELLANEOUS_1',
     description: 'Miscellaneous',
     active: true,
     nomisCode: 'MISC',
   },
-  {
+  MOBILE_PHONE_1: {
     familyCode: 'MOBILE_PHONE',
-    code: 'MOBILE_PHONE_1',
     description: 'Mobile phone',
     active: false,
     nomisCode: 'MOBILES',
   },
-  {
+  RADIO_COMPROMISE_1: {
     familyCode: 'RADIO_COMPROMISE',
-    code: 'RADIO_COMPROMISE_1',
     description: 'Radio compromise',
     active: false,
     nomisCode: 'RADIO_COMP',
   },
-  {
+  RELEASE_IN_ERROR_1: {
     familyCode: 'RELEASE_IN_ERROR',
-    code: 'RELEASE_IN_ERROR_1',
     description: 'Release in error',
     active: true,
     nomisCode: 'REL_ERROR',
   },
-  { familyCode: 'SELF_HARM', code: 'SELF_HARM_1', description: 'Self-harm', active: true, nomisCode: 'SELF_HARM' },
-  {
+  SELF_HARM_1: {
+    familyCode: 'SELF_HARM',
+    description: 'Self-harm',
+    active: true,
+    nomisCode: 'SELF_HARM',
+  },
+  TEMPORARY_RELEASE_FAILURE_1: {
     familyCode: 'TEMPORARY_RELEASE_FAILURE',
-    code: 'TEMPORARY_RELEASE_FAILURE_1',
     description: 'Temporary release failure',
     active: false,
     nomisCode: 'TRF',
   },
-  {
+  TEMPORARY_RELEASE_FAILURE_2: {
     familyCode: 'TEMPORARY_RELEASE_FAILURE',
-    code: 'TEMPORARY_RELEASE_FAILURE_2',
     description: 'Temporary release failure',
     active: false,
     nomisCode: 'TRF1',
   },
-  {
+  TEMPORARY_RELEASE_FAILURE_3: {
     familyCode: 'TEMPORARY_RELEASE_FAILURE',
-    code: 'TEMPORARY_RELEASE_FAILURE_3',
     description: 'Temporary release failure',
     active: false,
     nomisCode: 'TRF2',
   },
-  {
+  TEMPORARY_RELEASE_FAILURE_4: {
     familyCode: 'TEMPORARY_RELEASE_FAILURE',
-    code: 'TEMPORARY_RELEASE_FAILURE_4',
     description: 'Temporary release failure',
     active: true,
     nomisCode: 'TRF3',
   },
-  {
+  TOOL_LOSS_1: {
     familyCode: 'TOOL_LOSS',
-    code: 'TOOL_LOSS_1',
     description: 'Tool or equipment loss',
     active: true,
     nomisCode: 'TOOL_LOSS',
   },
-  {
+  TOOL_LOSS_2: {
     familyCode: 'TOOL_LOSS',
-    code: 'TOOL_LOSS_2',
     description: 'Tool or equipment loss',
     active: true,
     nomisCode: 'TOOL_LOSS2',
   },
-  {
+  UNLAWFUL_DETENTION_1: {
     familyCode: 'UNLAWFUL_DETENTION',
-    code: 'UNLAWFUL_DETENTION_1',
     description: 'Unlawful detention',
     active: true,
     nomisCode: 'UNLAW_DET',
   },
-] as const
-
-/** Types of reportable incidents */
-type TypeDetails = (typeof types)[number]
+} as const satisfies Record<string, TypeDetails>
 
 /** Codes for types of reportable incidents */
-export type Type = TypeDetails['code']
+export type Type = keyof typeof types
 
 /** Code to description mapping for types of reportable incidents */
 export const typesDescriptions: Record<Type, string> = Object.fromEntries(
-  types.map(type => [type.code, type.description]),
+  Object.entries(types).map(([typeCode, details]) => [typeCode, details.description]),
 ) as Record<Type, string>
 
 /**
  * NOMIS codes for Types of reportable incidents
  * @deprecated
  */
-export type NomisType = TypeDetails['nomisCode']
+const nomisCodes = Object.values(types).map(type => type.nomisCode)
+export type NomisType = (typeof nomisCodes)[number]
 
 /** Lookup for types of reportable incidents */
-export function getTypeDetails(code: string): TypeDetails | null {
-  return types.find(item => item.code === code) ?? null
+export function getTypeDetails(code: Type): TypeDetails | null {
+  return types[code] ?? null
 }
